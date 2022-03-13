614d2a75d96c6e6699f9a64158c434b650aff770
jaeryo2357 <jaeryo2357@naver.com>
2022-03-13 15:02:04 +0900

    🧸 add retrofit generate code and moshi convertor(#18)

--- app/src/main/java/com/mashup/di/NetworkModule.kt
+++ app/src/main/java/com/mashup/di/NetworkModule.kt
@@ -1,12 +1,59 @@
 package com.mashup.di
 
+import com.mashup.BuildConfig.DEBUG_MODE
+import com.mashup.network.interceptor.AuthInterceptor
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object NetworkModule {
     //TODO: replace base Url
     private const val BASE_URL = "https://sample.com"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(Date::class.java, Rfc3339DateJsonAdapter())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        if (DEBUG_MODE) {
+            okHttpClient.addInterceptor(HttpLoggingInterceptor())
+        }
+        return okHttpClient
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(): AuthInterceptor = AuthInterceptor()
 }

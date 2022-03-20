1fdec2f4f484b77c1906bfe2ab36339d5a03b7ac
jaeryo2357 <jaeryo2357@naver.com>
2022-03-20 23:17:44 +0900

    🛠 rename lifecycleScope and replace to fragmentExt(#22)

--- app/src/main/java/com/mashup/extensions/FragmentExt.kt
+++ app/src/main/java/com/mashup/extensions/FragmentExt.kt
@@ -4,7 +4,23 @@ import android.content.Context
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 fun Fragment.showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.flowViewLifecycleScope(
+    state: Lifecycle.State = Lifecycle.State.STARTED,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(state) {
+            block.invoke(this)
+        }
+    }
 }

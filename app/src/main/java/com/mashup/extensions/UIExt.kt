aa1c78ec13cc34ae83718ee7a45559c832d23412
jaeryo2357 <jaeryo2357@naver.com>
2022-05-15 18:12:11 +0900

    🧸 add button component(#26)

---
+++ app/src/main/java/com/mashup/extensions/UIExt.kt
@@ -0,0 +1,12 @@
+package com.mashup.extensions
+
+import android.content.Context
+import android.util.TypedValue
+
+fun Int.dp(context: Context): Int {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        context.resources.displayMetrics
+    ).toInt()
+}

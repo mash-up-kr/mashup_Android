aa3d85214daf2c3d3c98385a491bf440b8c07376
jaeryo2357 <jaeryo2357@naver.com>
2022-05-22 18:11:28 +0900

    🧸 add login viewmodel and userDataSource(#28)

--- app/src/main/java/com/mashup/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/ui/login/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.mashup.ui.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.mashup.R
 import com.mashup.base.BaseActivity
 import com.mashup.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+    private val viewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun initViews() {
+    }
+
+    override fun initObserves() {
+        viewModel.run {
+
+        }
     }
 
     override val layoutId: Int

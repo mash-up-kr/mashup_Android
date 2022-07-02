ecedc45f064b01ba2a83d43b9306c620be74a5f3
jaeryo2357 <jaeryo2357@naver.com>
2022-07-03 02:41:24 +0900

    🛠 add team color, rename primary to brand color (#40)

---
+++ compose/src/main/java/com/mashup/compose/colors/BrandColor.kt
@@ -0,0 +1,8 @@
+package com.mashup.compose.colors
+
+import androidx.compose.ui.graphics.Color
+
+val Brand600 = Color(0xFF5421E6)
+val Brand500 = Color(0xFF6A36FF)
+val Brand300 = Color(0xFFCDBFF6)
+val Brand100 = Color(0xFFF5F1FF)

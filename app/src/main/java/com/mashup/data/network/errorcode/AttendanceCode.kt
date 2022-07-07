4a4ceede87d39c662ae46326053d8b2c1f31bf2d
jaeryo2357 <jaeryo2357@naver.com>
2022-07-08 00:12:36 +0900

    🧸 add error code and Result class (#44)

---
+++ app/src/main/java/com/mashup/data/network/errorcode/AttendanceCode.kt
@@ -0,0 +1,31 @@
+package com.mashup.data.network.errorcode
+
+/**
+ * 이미 사용된 코드입니다.
+ */
+const val ATTENDANCE_CODE_DUPLICATED = "ATTENDANCE_CODE_DUPLICATED"
+
+/**
+ * 출석 코드가 존재하지 않습니다.
+ */
+const val ATTENDANCE_CODE_NOT_FOUND	= "ATTENDANCE_CODE_NOT_FOUND"
+
+/**
+ * 출석 정보가 존재하지 않습니다.
+ */
+const val ATTENDANCE_NOT_FOUND = "ATTENDANCE_NOT_FOUND"
+
+/**
+ * 이미 출석 체크를 했습니다.
+ */
+const val ATTENDANCE_ALREADY_CHECKED = "ATTENDANCE_ALREADY_CHECKED"
+
+/**
+ * 출석 체크 시간이 지났습니다.
+ */
+const val ATTENDANCE_TIME_OVER = "ATTENDANCE_TIME_OVER"
+
+/**
+ * 초대 코드가 존재하지 않습니다.
+ */
+const val INVITE_NOT_FOUND = "INVITE_NOT_FOUND"

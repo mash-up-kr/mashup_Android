965f00b45cbb1bc65f1393084bc35ec39f076681
jaeryo2357 <jaeryo2357@naver.com>
2022-07-09 17:58:23 +0900

    🧸 add network interceptor (#54)

---
+++ app/src/main/java/com/mashup/network/interceptor/AuthInterceptor.kt
@@ -0,0 +1,27 @@
+package com.mashup.network.interceptor
+
+import com.mashup.data.datastore.UserDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val userDataSource: UserDataSource
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain) = chain.proceed(
+        chain.request()
+            .newBuilder()
+            .apply {
+                runBlocking(Dispatchers.IO) {
+                    val token = userDataSource.token
+                    header(AUTHORIZATION_KEY, "Bearer $token")
+                }
+            }
+            .build()
+    )
+
+    companion object {
+        private const val AUTHORIZATION_KEY = "Authorization"
+    }
+}

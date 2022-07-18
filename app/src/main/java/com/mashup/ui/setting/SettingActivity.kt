825a3e505e17f89af7ca19bb748a29794d7ae9dd
yuchocopie <pointu341@gmail.com>
2022-07-19 04:02:48 +0900

    🧸 add setting withdrawal page

--- app/src/main/java/com/mashup/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/mashup/ui/setting/SettingActivity.kt
@@ -2,17 +2,38 @@ package com.mashup.ui.setting
 
 import android.content.Context
 import android.content.Intent
+import android.view.WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import com.mashup.R
 import com.mashup.base.BaseActivity
-import com.mashup.databinding.ActivityMainBinding
+import com.mashup.databinding.ActivitySettingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SettingActivity : BaseActivity<ActivityMainBinding>() {
-    override val layoutId = R.layout.activity_setting
+class SettingActivity : BaseActivity<ActivitySettingBinding>() {
 
     private val viewModel: SettingViewModel by viewModels()
+
+    override fun initViews() {
+        initButton()
+        window.setSoftInputMode(SOFT_INPUT_ADJUST_NOTHING);
+    }
+
+    private fun initButton() {
+        viewBinding.toolbar.setOnBackButtonClickListener {
+            onBackPressed()
+        }
+        viewBinding.btnWithdrawal.setOnClickListener {
+            changeOutContainerFragment(WithdrawalFragment.newInstance())
+        }
+    }
+
+    private fun changeOutContainerFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+        }.commit()
+    }
 
     companion object {
         fun start(context: Context?, action: Intent.() -> Unit = {}) {
@@ -21,4 +42,5 @@ class SettingActivity : BaseActivity<ActivityMainBinding>() {
         }
     }
 
+    override val layoutId = R.layout.activity_setting
 }

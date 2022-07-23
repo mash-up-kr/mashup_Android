1b6059e0fb6393280bbf4ab7ec66920c40e36da5
jaeryo2357 <jaeryo2357@naver.com>
2022-07-24 00:44:52 +0900

    🛠 중복코드 로직 추가하면서 viewModel 분리 (#67)

---
+++ app/src/main/java/com/mashup/data/dto/ValidResponse.kt
@@ -0,0 +1,10 @@
+package com.mashup.data.dto
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+class ValidResponse(
+    @field:Json(name = "valid")
+    val valid: Boolean,
+)

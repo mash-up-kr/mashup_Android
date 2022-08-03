6c02a925dd9c60e2695c6c17a60061c9f9a02f13
yuchocopie <pointu341@gmail.com>
2022-08-03 21:37:46 +0900

    🧸 마이페이지 출결 점수 서버 연결

--- app/src/main/java/com/mashup/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/mashup/ui/mypage/MyPageViewModel.kt
@@ -3,47 +3,109 @@ package com.mashup.ui.mypage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mashup.base.BaseViewModel
-import com.mashup.data.datastore.UserDataSource
+import com.mashup.data.repository.MemberRepository
 import com.mashup.data.repository.MyPageRepository
 import com.mashup.ui.model.ActivityHistory
 import com.mashup.ui.model.AttendanceModel
+import com.mashup.ui.model.Platform
 import com.mashup.ui.model.Profile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
-    private val myPageRepository: MyPageRepository,
-    private val userDataSource: UserDataSource
+    private val memberRepository: MemberRepository,
+    private val myPageRepository: MyPageRepository
 ) : BaseViewModel() {
 
     private val _attendanceList = MutableLiveData<List<AttendanceModel>>()
     val attendanceList: LiveData<List<AttendanceModel>> = _attendanceList
 
-
     init {
-        val test = listOf<AttendanceModel>(
-            AttendanceModel(0,0, Profile.EMPTY,12,null),
-            AttendanceModel(0,1, Profile.EMPTY,12,null),
-            AttendanceModel(0,4, Profile.EMPTY,12,null),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PLACEHOLDER_HISTORY, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PRESENTATION, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PROJECT_FAIL, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PROJECT_SUCCESS, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.ATTENDANCE, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.ABSENT, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.LATE, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.HACKATHON_PREPARE, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PROJECT_LEADER, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.PROJECT_SUB_LEADER, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,4, Profile.EMPTY,13,null),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.STAFF, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.MASHUP_LEADER, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.MASHUP_SUB_LEADER, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.TECH_BLOG_WRITE, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-            AttendanceModel(0,3, Profile.EMPTY,12, ActivityHistory(attendanceType = AttendanceType.MASHUP_CONTENTS_WRITE, totalScore = 103, detail = "3차 전체 세미나", date = "2022.02.03",)),
-        )
-        _attendanceList.value = test
+        getMember()
+    }
+
+    private fun getMember() = mashUpScope {
+        try {
+
+            val response = memberRepository.getMember()
+
+            if (response.isSuccess()) {
+                getScore(
+                    Profile(
+                        platform = Platform.getPlatform(response.data?.platform),
+                        name = response.data?.name.toString(),
+                        score = 0.0
+                    )
+                )
+            }
+        } catch (ignore: Exception) {
+        }
+    }
+
+    private fun getScore(profile: Profile) = mashUpScope {
+        try {
+            val response = myPageRepository.getScoreHistory()
+            if (response.isSuccess()) {
+                val list = mutableListOf<AttendanceModel>()
+
+                if (response.data?.isNotEmpty() == true) {
+                    list += AttendanceModel(
+                        0,
+                        MyPageAdapterType.TITLE,
+                        profile.also {
+                            it.score = response.data.first().totalScore
+                        }, null, null
+                    )
+
+                    list += AttendanceModel(
+                        0, MyPageAdapterType.SCORE,
+                        profile.also {
+                            it.score = response.data.first().totalScore
+                        }, null, null
+                    )
+                    response.data.forEach { it ->
+                        list += AttendanceModel(
+                            0, MyPageAdapterType.LIST_LEVEL, null, it.generationNumber, null
+                        )
+                        list += it.scoreDetails.map { score ->
+                            AttendanceModel(
+                                id = 0,
+                                MyPageAdapterType.LIST_ITEM,
+                                null,
+                                it.generationNumber,
+                                ActivityHistory(
+                                    attendanceType = AttendanceType.getAttendanceType(score.scoreType),
+                                    totalScore = score.cumulativeScore,
+                                    detail = score.scoreName,
+                                    date = score.date
+                                )
+                            )
+                        }
+
+                    }
+                } else {
+                    list.addAll(
+                        listOf(
+                            AttendanceModel(
+                                0, MyPageAdapterType.TITLE, profile, null, null
+                            ),
+                            AttendanceModel(
+                                0, MyPageAdapterType.SCORE, profile, null, null
+                            ),
+                            AttendanceModel(
+                                0, MyPageAdapterType.LIST_NONE, profile, null, null
+                            )
+                        )
+                    )
+
+                }
+                _attendanceList.postValue(list)
+            } else {
+
+            }
+        } catch (ignore: Exception) {
+        }
     }
 
     override fun handleErrorCode(code: String) {

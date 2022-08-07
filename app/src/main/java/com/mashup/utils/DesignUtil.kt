cb228164c44f4a44fdd257debff6898b437b6b5a
jaeryo2357 <jaeryo2357@naver.com>
2022-08-07 14:19:22 +0900

    🛠 메인 상태바 변경하는 로직 추가

--- app/src/main/java/com/mashup/utils/DesignUtil.kt
+++ app/src/main/java/com/mashup/utils/DesignUtil.kt
@@ -91,33 +91,26 @@ object DesignUtil {
 
     @JvmStatic
     @BindingAdapter("attendanceScore")
-    fun AppCompatTextView.setAttendanceScore(attendanceType: AttendanceType) {
-        attendanceType.score.let {
-            text = if (it == 0.0) {
-                setTextColor(ContextCompat.getColor(context, R.color.gray500))
-                "0 점"
+    fun AppCompatTextView.setAttendanceScore(attendanceScore: Double) {
+        text = if (attendanceScore == 0.0) {
+            setTextColor(ContextCompat.getColor(context, R.color.gray500))
+            "0점"
+        } else {
+            val score: Number =
+                if (attendanceScore % 1 == 0.0) attendanceScore.toInt() else attendanceScore
+            if (attendanceScore > 0) {
+                setTextColor(ContextCompat.getColor(context, R.color.blue500))
+                "+${score}점"
             } else {
-                val score = if (it % 1 == 0.0) it.toInt() else it
-                if (it > 0) {
-                    setTextColor(ContextCompat.getColor(context, R.color.blue500))
-                    "+${score}점"
-                } else {
-                    setTextColor(ContextCompat.getColor(context, R.color.red500))
-                    "${score}점"
-                }
+                setTextColor(ContextCompat.getColor(context, R.color.red500))
+                "${score}점"
             }
         }
     }
 
     @JvmStatic
-    @BindingAdapter("attendanceTitle")
-    fun AppCompatTextView.setAttendanceTitle(attendanceType: AttendanceType) {
-        text = attendanceType.title
-    }
-
-    @JvmStatic
     @BindingAdapter("attendanceIcon")
     fun ImageView.setAttendanceIcon(attendanceType: AttendanceType) {
-        setImageDrawable(ContextCompat.getDrawable(context,attendanceType.resourceId))
+        setImageDrawable(ContextCompat.getDrawable(context, attendanceType.resourceId))
     }
 }

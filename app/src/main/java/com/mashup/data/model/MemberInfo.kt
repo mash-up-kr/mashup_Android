bf84cc3e9d5ead646d7b242dfc188e6c5bbb7f1e
jaeryo2357 <jaeryo2357@naver.com>
2022-09-06 03:16:50 +0900

    🛠 crew final attendance not yet 적용

--- app/src/main/java/com/mashup/data/model/MemberInfo.kt
+++ app/src/main/java/com/mashup/data/model/MemberInfo.kt
@@ -4,6 +4,9 @@ import com.mashup.ui.attendance.model.AttendanceStatus
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * AttendanceInfoResponse랑 합치면 좋을 듯
+ */
 @JsonClass(generateAdapter = true)
 data class MemberInfo(
     @field:Json(name = "name")
@@ -11,17 +14,22 @@ data class MemberInfo(
     @field:Json(name = "attendanceInfos")
     val attendanceInfos: List<AttendanceInfo>
 ) {
+    private fun getAttendanceStatus(position: Int): AttendanceStatus {
+        return AttendanceStatus.getAttendanceStatus(attendanceInfos.getOrNull(position)?.status)
+    }
+
     fun getFinalAttendance(): AttendanceStatus {
         return when {
-            attendanceInfos.isEmpty() -> {
-                AttendanceStatus.NOT_YET
-            }
-            attendanceInfos[0].status.uppercase() == "ATTENDANCE"
-                && attendanceInfos[1].status.uppercase() == "ATTENDANCE" -> {
+            getAttendanceStatus(0) == AttendanceStatus.ATTENDANCE
+                && getAttendanceStatus(1) == AttendanceStatus.ATTENDANCE -> {
                 AttendanceStatus.ATTENDANCE
             }
-            attendanceInfos[0].status.uppercase() == "ABSENT"
-                || attendanceInfos[1].status.uppercase() == "ABSENT" -> {
+            getAttendanceStatus(0) == AttendanceStatus.NOT_YET
+                || getAttendanceStatus(1) == AttendanceStatus.NOT_YET -> {
+                AttendanceStatus.NOT_YET
+            }
+            getAttendanceStatus(0) == AttendanceStatus.ABSENT
+                || getAttendanceStatus(1) == AttendanceStatus.ABSENT -> {
                 AttendanceStatus.ABSENT
             }
             else -> {

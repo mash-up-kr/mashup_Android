c8d68597bb0038be41ab69c28c6e257b07024401
jaeryo2357 <jaeryo2357@naver.com>
2022-10-31 02:05:32 +0900

    🧸 attendance 취소 Ui 추가 (#133)

--- core/common/src/main/java/com/mashup/core/common/extensions/TextViewExt.kt
+++ core/common/src/main/java/com/mashup/core/common/extensions/TextViewExt.kt
@@ -6,3 +6,11 @@ import android.widget.TextView
 fun TextView.setUnderLine() {
     paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
 }
+
+fun TextView.addCancelLine() {
+    paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+}
+
+fun TextView.removeCancelLine() {
+    paintFlags = paintFlags xor Paint.STRIKE_THRU_TEXT_FLAG
+}

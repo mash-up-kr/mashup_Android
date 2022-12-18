af8eb25889adb921b7d345b7cdbf18fb97de39b4
jaeryo2357 <jaeryo2357@naver.com>
2022-12-18 23:54:04 +0900

    🐛 authInterceptor에서 토큰 만료 에러 핸들링하기(#161)

--- app/src/main/java/com/mashup/network/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/mashup/network/interceptor/AuthInterceptor.kt
@@ -1,26 +1,51 @@
 package com.mashup.network.interceptor
 
 import com.mashup.data.datastore.UserDataSource
+import com.mashup.network.errorcode.UNAUTHORIZED
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.json.JSONObject
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
     private val userDataSource: UserDataSource
 ) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain) = chain.proceed(
-        chain.request()
-            .newBuilder()
-            .apply {
-                runBlocking(Dispatchers.IO) {
-                    userDataSource.token?.let { token ->
-                        header(AUTHORIZATION_KEY, "Bearer $token")
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(
+            chain.request()
+                .newBuilder()
+                .apply {
+                    runBlocking(Dispatchers.IO) {
+                        userDataSource.token?.let { token ->
+                            header(AUTHORIZATION_KEY, "Bearer $token")
+                        }
                     }
                 }
-            }
+                .build()
+        )
+        val responseJson = extractResponseJson(response)
+        checkApiStatusCode(responseJson)
+
+        return response.newBuilder()
+            .body(responseJson.toString().toResponseBody())
             .build()
-    )
+    }
+
+    private fun extractResponseJson(response: Response): JSONObject {
+        val jsonString = response.body?.string() ?: "{}"
+        return JSONObject(jsonString)
+    }
+
+    private fun checkApiStatusCode(jsonObject: JSONObject) {
+        when (jsonObject.get("code")) {
+            UNAUTHORIZED -> {
+                userDataSource.token = ""
+            }
+        }
+    }
 
     companion object {
         private const val AUTHORIZATION_KEY = "Authorization"

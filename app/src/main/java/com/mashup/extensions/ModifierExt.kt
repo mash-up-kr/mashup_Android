37a20b0766990bd4ba4b079549a52b063fc9f3d5
정민지 <s2018s38@e-mirim.hs.kr>
2022-12-18 23:24:58 +0900

    🧸 Composable 함수에 그림자를 커스텀하기 위한 확장함수 생성 및 적용(#153)

---
+++ app/src/main/java/com/mashup/extensions/ModifierExt.kt
@@ -0,0 +1,60 @@
+package com.mashup.extensions
+
+import android.graphics.BlurMaskFilter
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * 그림자를 커스텀하기 위한 확장 함수.
+ *
+ * @param color 그림자의 색을 지정합니다.
+ * @param borderRadius 그림자의 둥글기를 지정합니다.
+ * @param blurRadius 그림자의 흐림 정도를 지정합니다.
+ * @param offsetX 그림자의 X축을 조정합니다.
+ * @param offsetY 그림자의 Y축을 조정합니다.
+ * @param spread 그림자가 퍼지는 정도를 조정합니다.
+ * @param modifier Modifier
+ */
+fun Modifier.shadow(
+    color: Color = Color.Black,
+    borderRadius: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    spread: Dp = 0.dp,
+    modifier: Modifier = Modifier
+) = this.then(
+    modifier.drawBehind {
+        this.drawIntoCanvas {
+            val paint = Paint()
+            val frameworkPaint = paint.asFrameworkPaint()
+            val spreadPixel = spread.toPx()
+            val leftPixel = (0f - spreadPixel) + offsetX.toPx()
+            val topPixel = (0f - spreadPixel) + offsetY.toPx()
+            val rightPixel = (this.size.width + spreadPixel)
+            val bottomPixel = (this.size.height + spreadPixel)
+
+            if (blurRadius != 0.dp) {
+                frameworkPaint.maskFilter =
+                    (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
+            }
+
+            frameworkPaint.color = color.toArgb()
+            it.drawRoundRect(
+                left = leftPixel,
+                top = topPixel,
+                right = rightPixel,
+                bottom = bottomPixel,
+                radiusX = borderRadius.toPx(),
+                radiusY = borderRadius.toPx(),
+                paint
+            )
+        }
+    }
+)

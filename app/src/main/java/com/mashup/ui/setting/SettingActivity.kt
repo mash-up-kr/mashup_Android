d276011cc56d09d12ac189500006d7537ca8fcdc
jaeryo2357 <jaeryo2357@naver.com>
2023-01-07 02:57:37 +0900

    🧸 설정화면 GA Event 정의 (#113)

--- app/src/main/java/com/mashup/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/mashup/ui/setting/SettingActivity.kt
@@ -3,12 +3,21 @@ package com.mashup.ui.setting
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.activity.viewModels
 import com.mashup.R
+import com.mashup.URL
 import com.mashup.base.BaseActivity
 import com.mashup.constant.EXTRA_ANIMATION
+import com.mashup.constant.LOG_DELETE_USER
+import com.mashup.constant.LOG_LOGOUT
+import com.mashup.constant.LOG_SNS_FACEBOOK
+import com.mashup.constant.LOG_SNS_INSTAGRAM
+import com.mashup.constant.LOG_SNS_MASHUP_HOME
+import com.mashup.constant.LOG_SNS_MASHUP_RECRUIT
+import com.mashup.constant.LOG_SNS_TISTORY
+import com.mashup.constant.LOG_SNS_YOUTUBE
 import com.mashup.core.common.model.NavigationAnimationType
 import com.mashup.core.common.widget.CommonDialog
 import com.mashup.core.ui.theme.MashUpTheme
@@ -31,7 +40,7 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
             MashUpTheme {
                 SettingScreen(
                     modifier = Modifier.fillMaxSize(),
-                    onLogout = this::showLogoutDialog,
+                    onLogout = this::onClickLogoutButton,
                     onDeleteUser = this::moveToDeleteAccount,
                     onClickSNS = this::onClickSNS
                 )
@@ -52,6 +61,11 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
         viewBinding.toolbar.setOnBackButtonClickListener {
             onBackPressed()
         }
+    }
+
+    private fun onClickLogoutButton() {
+        sendEvent(LOG_LOGOUT)
+        showLogoutDialog()
     }
 
     private fun showLogoutDialog() {
@@ -76,12 +90,37 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
     }
 
     private fun moveToDeleteAccount() {
+        sendEvent(LOG_DELETE_USER)
         startActivity(
             WithdrawalActivity.newInstance(this)
         )
     }
 
     private fun onClickSNS(link: String) {
+        val eventLog = when (link) {
+            URL.FACEBOOK -> {
+                LOG_SNS_FACEBOOK
+            }
+            URL.INSTAGRAM -> {
+                LOG_SNS_INSTAGRAM
+            }
+            URL.TISTORY -> {
+                LOG_SNS_TISTORY
+            }
+            URL.YOUTUBE -> {
+                LOG_SNS_YOUTUBE
+            }
+            URL.MASHUP_UP_HOME -> {
+                LOG_SNS_MASHUP_HOME
+            }
+            URL.MASHUP_UP_RECRUIT -> {
+                LOG_SNS_MASHUP_RECRUIT
+            }
+            else -> {
+                null
+            }
+        }
+        eventLog?.run { sendEvent(this) }
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
     }
 

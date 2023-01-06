8be4f14eb73c6532867dcf262de46ae59d9bddac
jaeryo2357 <jaeryo2357@naver.com>
2023-01-07 02:57:37 +0900

    🧸 설정화면 GA Event 정의 (#113)

--- app/src/main/java/com/mashup/constant/UserActionLogs.kt
+++ app/src/main/java/com/mashup/constant/UserActionLogs.kt
@@ -2,3 +2,13 @@ package com.mashup.constant
 
 const val LOG_LOGIN = "login"
 const val LOG_SIGN_UP = "signup"
+
+// SettingScreen
+const val LOG_LOGOUT = "logout"
+const val LOG_DELETE_USER = "delete_user"
+const val LOG_SNS_FACEBOOK = "facebook"
+const val LOG_SNS_INSTAGRAM = "instagram"
+const val LOG_SNS_TISTORY = "tistory"
+const val LOG_SNS_YOUTUBE = "youtube"
+const val LOG_SNS_MASHUP_HOME = "mashup_home"
+const val LOG_SNS_MASHUP_RECRUIT = "mashup_recruit"

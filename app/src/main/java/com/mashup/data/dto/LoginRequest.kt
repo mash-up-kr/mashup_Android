3239fef86b6b311fc854f533023629757eb8beb9
jaeryo2357 <jaeryo2357@naver.com>
2023-01-08 19:48:31 +0900

    🧸 로그인 시 fcm token 올리기(#181)

--- app/src/main/java/com/mashup/data/dto/LoginRequest.kt
+++ app/src/main/java/com/mashup/data/dto/LoginRequest.kt
@@ -8,5 +8,9 @@ data class LoginRequest(
     @field:Json(name = "identification")
     val identification: String,
     @field:Json(name = "password")
-    val password: String
+    val password: String,
+    @field:Json(name = "fcmToken")
+    val fcmToken: String,
+    @field:Json(name = "osType")
+    val osType: String = "ANDROID"
 )

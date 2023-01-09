789c5079242285d35f2edfcb993e6497f0689013
joo <tjrwn0716@naver.com>
2023-01-09 23:24:58 +0900

    [Refactoring] QR Code GA Logging

--- app/src/main/java/com/mashup/constant/UserActionLogs.kt
+++ app/src/main/java/com/mashup/constant/UserActionLogs.kt
@@ -2,3 +2,9 @@ package com.mashup.constant
 
 const val LOG_LOGIN = "login"
 const val LOG_SIGN_UP = "signup"
+
+const val LOG_QR = "qr"
+const val LOG_QR_SUCCESS = "qr_success"
+const val LOG_QR_DONE = "qr_done"
+const val LOG_QR_TIME_FAIL = "qr_time_fail"
+const val LOG_QR_WRONG = "qr_wrong"

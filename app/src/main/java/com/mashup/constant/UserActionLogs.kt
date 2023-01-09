2a0152a0d3f377547b6c3c5f8e0e013feea74bc3
jaeryo2357 <jaeryo2357@naver.com>
2023-01-09 23:26:43 +0900

    🧸 이벤트 리스트 GA 추가(#178)

--- app/src/main/java/com/mashup/constant/UserActionLogs.kt
+++ app/src/main/java/com/mashup/constant/UserActionLogs.kt
@@ -2,3 +2,7 @@ package com.mashup.constant
 
 const val LOG_LOGIN = "login"
 const val LOG_SIGN_UP = "signup"
+
+const val LOG_SCHEDULE_LIST_REFRESH = "refresh"
+const val LOG_SCHEDULE_STATUS_CONFIRM = "status_confirm"
+const val LOG_SCHEDULE_EVENT_DETAIL = "event_detail"

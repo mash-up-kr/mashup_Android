887d1eb31f89d99a7ea022b6c5a3be4847a5aae5
jaeryo2357 <jaeryo2357@naver.com>
2023-04-03 22:11:30 +0900

    🧸 #268 sensor module 구현

---
+++ core/shake/src/main/java/com/mashup/shake/di/SensorModule.kt
@@ -0,0 +1,25 @@
+package com.mashup.shake.di
+
+import android.content.Context
+import android.hardware.SensorManager
+import com.mashup.shake.ShakeDetector
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class SensorModule {
+    @Provides
+    @Singleton
+    fun providesShakeDetector(
+        @ApplicationContext context: Context
+    ): ShakeDetector {
+        return ShakeDetector(
+            sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        )
+    }
+}

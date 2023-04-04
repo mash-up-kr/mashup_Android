87861e0e4f8c4c543cac1ec167aa79c357dc4769
jaeryo2357 <jaeryo2357@naver.com>
2023-04-05 01:09:37 +0900

    🧸 #264 업데이트 유도 팝업 구현

---
+++ app/src/main/java/com/mashup/ui/splash/SplashViewModel.kt
@@ -0,0 +1,43 @@
+package com.mashup.ui.splash
+
+import android.content.Context
+import androidx.core.content.pm.PackageInfoCompat
+import com.mashup.base.BaseViewModel
+import com.mashup.core.firebase.FirebaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val firebaseRepository: FirebaseRepository
+) : BaseViewModel() {
+
+    private val _onLowerAppVersion = MutableSharedFlow<Unit>()
+    val onLowerAppVersion: SharedFlow<Unit> = _onLowerAppVersion
+
+    private val _onFinishInit = MutableSharedFlow<Unit>()
+    val onFinishInit: SharedFlow<Unit> = _onFinishInit
+
+    fun checkAppVersion(context: Context) = mashUpScope {
+        try {
+            val localVersionCode = PackageInfoCompat.getLongVersionCode(
+                context.packageManager.getPackageInfo(context.packageName, 0)
+            )
+            val resentAppVersion = firebaseRepository.getRecentAppVersion()
+            if (localVersionCode < resentAppVersion) {
+                _onLowerAppVersion.emit(Unit)
+            } else {
+                delay(2000L)
+                _onFinishInit.emit(Unit)
+            }
+        } catch (cancelException: CancellationException) {
+            throw cancelException
+        } catch (ignore: Exception) {
+        }
+    }
+}

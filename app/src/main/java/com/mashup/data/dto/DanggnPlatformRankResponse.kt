eef27ae8dfcd589ba2f95192ce1d0bb4881cc18e
Pita <pita.pat@kakaopaycorp.com>
2023-04-08 14:57:30 +0900

    [🧸feat] api response 수정 (platform api)

--- app/src/main/java/com/mashup/data/dto/DanggnPlatformRankResponse.kt
+++ app/src/main/java/com/mashup/data/dto/DanggnPlatformRankResponse.kt
@@ -1,31 +1,12 @@
 package com.mashup.data.dto
+
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class DanggnPlatformRankResponse(
-    @Json(name = "code")
-    val code: String,
-    @Json(name = "data")
-    val `data`: List<Data>,
-    @Json(name = "message")
-    val message: String,
-    @Json(name = "page")
-    val page: Page
-) {
-    data class Data(
-        @Json(name = "platform")
-        val platform: String,
-        @Json(name = "totalShakeScore")
-        val totalShakeScore: Int
-    )
-
-    data class Page(
-        @Json(name = "number")
-        val number: Int,
-        @Json(name = "size")
-        val size: Int,
-        @Json(name = "totalCount")
-        val totalCount: Int
-    )
-}
+    @Json(name = "platform")
+    val platform: String,
+    @Json(name = "totalShakeScore")
+    val totalShakeScore: Int
+)

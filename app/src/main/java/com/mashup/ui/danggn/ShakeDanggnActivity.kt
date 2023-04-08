c70e9861ad8a04ffa339dafc5a79f98f5f1acec9
정민지 <s2018s38@e-mirim.hs.kr>
2023-04-09 00:32:56 +0900

    🧸 #277 BaseActivity 상속하도록 구현

--- app/src/main/java/com/mashup/ui/danggn/ShakeDanggnActivity.kt
+++ app/src/main/java/com/mashup/ui/danggn/ShakeDanggnActivity.kt
@@ -2,14 +2,17 @@ package com.mashup.ui.danggn
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import com.mashup.R
+import com.mashup.base.BaseActivity
+import com.mashup.databinding.ActivityShakeDanggnBinding
 
-class ShakeDanggnActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
+class ShakeDanggnActivity : BaseActivity<ActivityShakeDanggnBinding>() {
+    override val layoutId: Int = R.layout.activity_shake_danggn
+
+    override fun initViews() {
+        super.initViews()
+
+        viewBinding.shakeDanggnScreen.setContent {
 
         }
     }

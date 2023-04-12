8c604dea61bb5a5555e5aba8f8058c5c6f9162d5
정민지 <s2018s38@e-mirim.hs.kr>
2023-04-13 00:56:58 +0900

    🧸 당근 흔들기(DanggnShakeContent), 당근 랭킹(DanggnRankingContent) 컴포저블 각각 생성
    - UI 작업하기 편하도록 컨텐츠 별 컴포저블 생성
    - 당근 흔들기: DanggnShakeContent
    - 당근 랭킹: DanggnRankingContent

---
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/shake/DanggnShakeContent.kt
@@ -0,0 +1,21 @@
+package com.mashup.feature.danggn.shake
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DanggnShakeContent(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    ) {
+
+    }
+}

2b12cbd582e30623685601dcec76f6d829b64e62
Ahn-seokjoo <tjrwn0716@naver.com>
2023-04-18 00:19:23 +0900

    [feature] Ranking 임시 커밋

--- feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnRankingContent.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnRankingContent.kt
@@ -1,14 +1,110 @@
 package com.mashup.feature.danggn.ranking
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import com.mashup.core.ui.colors.Black
+import com.mashup.core.ui.colors.Gray400
+import com.mashup.core.ui.colors.White
+import com.mashup.core.ui.theme.MashUpTheme
+import com.mashup.core.ui.typography.Title1
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun DanggnRankingContent(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
+    val pages = listOf("크루원", "플랫폼 팀")
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
 
+    Column(modifier = modifier.background(White)) {
+        Text(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 24.dp)
+                .fillMaxWidth(),
+            text = "랭킹",
+            style = Title1,
+        )
+        TabRow(
+            modifier = Modifier,
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    modifier = Modifier
+                        .pagerTabIndicatorOffset(
+                            pagerState = pagerState,
+                            tabPositions = tabPositions
+                        )
+                        .padding(horizontal = 60.dp),
+                    color = Black
+                )
+            }
+        ) {
+            pages.forEachIndexed { index, title ->
+                Tab(
+                    modifier = Modifier
+                        .background(White),
+                    text = {
+                        MashUpPagerColorAnimator(title, pagerState.currentPage == index)
+                    },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.scrollToPage(index)
+                        }
+                    })
+            }
+        }
+        HorizontalPager(count = pages.size, state = pagerState) { page ->
+            // ranking ui
+
+        }
+    }
+}
+
+@Composable
+private fun MashUpPagerColorAnimator(
+    title: String,
+    selected: Boolean
+) {
+    val textColorAnimation by animateColorAsState(
+        targetValue = if (selected) Black else Gray400
+    )
+    Text(
+        text = title,
+        textAlign = TextAlign.Start,
+        color = textColorAnimation
+    )
+}
+
+@Preview("Ranking Preview")
+@Composable
+fun MashUpRankingPreview() {
+    MashUpTheme {
+        DanggnRankingContent()
     }
 }

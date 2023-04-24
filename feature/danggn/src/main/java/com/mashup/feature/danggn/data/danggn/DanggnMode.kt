f9a369bc745456a1010ef549e9e8d417ce7ea1bb
jaeryo2357 <jaeryo2357@naver.com>
2023-04-25 00:10:08 +0900

    🧸 #292 황금 당근 적용

---
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnMode.kt
@@ -0,0 +1,22 @@
+package com.mashup.feature.danggn.data.danggn
+
+import kotlin.random.Random
+
+sealed interface DanggnMode {
+    fun getNextScore(currentScore: Int): Int
+}
+
+object NormalDanggnMode : DanggnMode {
+    override fun getNextScore(currentScore: Int) = currentScore + 1
+
+    /**
+     * @param goldenDanggnPercent 0 util 100
+     */
+    fun canSwitchToGoldenDanggnMode(goldenDanggnPercent: Int): Boolean {
+        return Random.nextInt(100) > goldenDanggnPercent
+    }
+}
+
+object GoldenDanggnMode : DanggnMode {
+    override fun getNextScore(currentScore: Int) = currentScore + 100
+}
---
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnShaker.kt
@@ -0,0 +1,149 @@
+package com.mashup.feature.danggn.data.danggn
+
+import android.util.Log
+import com.mashup.feature.danggn.data.ShakeDetector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicInteger
+import javax.inject.Inject
+
+/**
+ * ShakeDetector의 shake 이벤트를 탐지하여 콤보 카운트를 계산합니다.
+ */
+class DanggnShaker @Inject constructor(
+    private val shakeDetector: ShakeDetector
+) {
+    private val danggnModeChannel = Channel<DanggnMode>(Channel.UNLIMITED)
+    private val shakerStateChannel = Channel<DanggnShakerState>(Channel.UNLIMITED)
+    private val comboScoreChannel = Channel<Int>(Channel.UNLIMITED)
+
+    private var danggnShakerScope: CoroutineScope? = null
+    private var debounceDetectorJob: Job? = null
+
+    private var lastShakeTime: Long = 0
+    private var comboScore = AtomicInteger()
+
+    private var danggnMode: DanggnMode = NormalDanggnMode
+
+    fun start(
+        threshold: Int,
+        interval: Long,
+        goldenDanggnPercent: Int = 50
+    ) {
+        danggnShakerScope = CoroutineScope(Dispatchers.Default)
+        collectComboFlow()
+        runDanggnModeController()
+        runComboScoreController(goldenDanggnPercent)
+        shakeDetector.startListening(
+            threshold = threshold,
+            interval = interval,
+            onShakeDevice = {
+                Log.d("DanggnShake", "onShake")
+                danggnShakerScope?.launch {
+                    Log.d("shakerState", "thread: ${Thread.currentThread().name}")
+                    val newComboScore = comboScore.updateAndGet(danggnMode::getNextScore)
+                    shakerStateChannel.send(
+                        DanggnShakerState.Combo(
+                            mode = danggnMode,
+                            score = newComboScore
+                        )
+                    )
+                    comboScoreChannel.send(newComboScore)
+                }
+            }
+        )
+    }
+
+    fun getDanggnShakeState(): Flow<DanggnShakerState> = shakerStateChannel.consumeAsFlow()
+
+    fun stop() {
+        danggnShakerScope?.cancel()
+        debounceDetectorJob?.cancel()
+        shakeDetector.stopListening()
+        clearFlag()
+    }
+
+    private fun collectComboFlow() {
+        danggnShakerScope?.launch {
+            comboScoreChannel.consumeAsFlow().debounce(COMBO_TERM_DURATION)
+                .collectLatest {
+                    val lastScore = comboScore.getAndSet(0)
+                    shakerStateChannel.send(
+                        DanggnShakerState.End(
+                            mode = danggnMode,
+                            lastScore = lastScore
+                        )
+                    )
+                    shakerStateChannel.send(DanggnShakerState.Idle(mode = danggnMode))
+                }
+        }
+    }
+
+    private fun runComboScoreController(goldenDanggnPercent: Int) {
+        danggnShakerScope?.launch {
+            while (danggnShakerScope?.isActive == true) {
+                comboScoreChannel.receive()
+                if (
+                    (danggnMode as? NormalDanggnMode)?.canSwitchToGoldenDanggnMode(
+                        goldenDanggnPercent
+                    ) == true
+                ) {
+                    danggnModeChannel.send(GoldenDanggnMode)
+                }
+            }
+        }
+    }
+
+    private fun runDanggnModeController() {
+        danggnShakerScope?.launch {
+            while (danggnShakerScope?.isActive == true) {
+                danggnMode = danggnModeChannel.receiveMode()
+
+                if (danggnMode is GoldenDanggnMode) {
+                    delay(TIME_GOLDEN_STAGE)
+                    danggnModeChannel.send(NormalDanggnMode)
+                }
+            }
+        }
+    }
+
+    private fun clearFlag() {
+        lastShakeTime = 0
+        comboScore.set(0)
+    }
+
+    private suspend fun Channel<DanggnMode>.receiveMode(): DanggnMode {
+        return if (isEmpty) NormalDanggnMode else receive()
+    }
+
+    companion object {
+        private const val COMBO_TERM_DURATION = 2000L
+        private const val TIME_GOLDEN_STAGE = 3000L
+    }
+}
+
+sealed interface DanggnShakerState {
+    data class Idle(
+        val mode: DanggnMode = NormalDanggnMode
+    ) : DanggnShakerState
+
+    data class Combo(
+        val score: Int,
+        val mode: DanggnMode = NormalDanggnMode
+    ) : DanggnShakerState
+
+    data class End(
+        val lastScore: Int,
+        val mode: DanggnMode = NormalDanggnMode
+    ) : DanggnShakerState
+}

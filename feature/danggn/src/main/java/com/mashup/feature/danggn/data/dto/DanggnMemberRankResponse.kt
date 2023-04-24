08e8ec2fb0c03521300dab70640fb34a4474966d
Ahn-seokjoo <tjrwn0716@naver.com>
2023-04-24 23:36:40 +0900

    conflict 해결

--- feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnMemberRankResponse.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnMemberRankResponse.kt
@@ -12,11 +12,3 @@ data class DanggnMemberRankResponse(
     @Json(name = "totalShakeScore")
     val totalShakeScore: Int
 )
-
-@JsonClass(generateAdapter = true)
-data class DanggnAllMemberRankResponse(
-    @Json(name = "danggnMemberRankDataList")
-    val allMemberRankList: List<DanggnMemberRankResponse>,
-    @Json(name = "limit")
-    val limit: Int,
-)

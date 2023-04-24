5ff738bee6f5bd0d0637ba51757cf7e336e46c44
정민지 <s2018s38@e-mirim.hs.kr>
2023-04-24 22:11:55 +0900

    🧸 당근 흔들기 정보 화면 추가
    - 당근 흔들기 화면에서 진입할 수 있는 화면 추가(DanggnInfoActivity)
    - Typography에 SubTitle3 스타일 추가

---
+++ app/src/main/java/com/mashup/ui/danggn/DanggnInfoActivity.kt
@@ -0,0 +1,34 @@
+package com.mashup.ui.danggn
+
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.mashup.R
+import com.mashup.base.BaseActivity
+import com.mashup.core.ui.theme.MashUpTheme
+import com.mashup.databinding.ActivityDanggnInfoBinding
+import com.mashup.feature.danggn.DanggnInfoScreen
+
+class DanggnInfoActivity : BaseActivity<ActivityDanggnInfoBinding>() {
+    override val layoutId: Int = R.layout.activity_danggn_info
+
+    override fun initViews() {
+        super.initViews()
+
+        viewBinding.shakeDanggnScreen.setContent {
+            MashUpTheme {
+                DanggnInfoScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    onClickBackButton = { onBackPressed() },
+                )
+            }
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, DanggnInfoActivity::class.java).apply {
+
+        }
+    }
+}

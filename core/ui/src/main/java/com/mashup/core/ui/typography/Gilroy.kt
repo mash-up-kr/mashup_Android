0b5c05c3da69c5077521fafe62c74049f44c6557
Ahn-seokjoo <tjrwn0716@naver.com>
2023-04-27 00:30:17 +0900

    [feature] 랭킹 3위 이상의 값 추가 및 디바이더 추가

---
+++ core/ui/src/main/java/com/mashup/core/ui/typography/Gilroy.kt
@@ -0,0 +1,27 @@
+package com.mashup.core.ui.typography
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.mashup.core.common.R
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+private val gilroyFontFamily = FontFamily(
+    Font(R.font.gilroy_light),
+    Font(R.font.gilroy_extrabold, FontWeight.ExtraBold),
+)
+
+val GilroyNormal = TextStyle(
+    fontFamily = gilroyFontFamily,
+    fontSize = 16.sp,
+    letterSpacing = (-0.01).em
+)
+
+val GilroyBold = TextStyle(
+    fontFamily = gilroyFontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    letterSpacing = (-0.01).em
+)

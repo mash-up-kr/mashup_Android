b77ceefdc81d09ac3bc2610d32b1126bb540342b
jaeryo2357 <jaeryo2357@naver.com>
2023-04-28 00:07:14 +0900

    🐛 #304 황금 당근 확률 로직 수정

--- feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnMode.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnMode.kt
@@ -10,10 +10,10 @@ object NormalDanggnMode : DanggnMode {
     override fun getNextScore(currentScore: Int) = currentScore + 1
 
     /**
-     * @param goldenDanggnPercent 0 util 100
+     * @param goldenDanggnPercent 1 util 100
      */
     fun canSwitchToGoldenDanggnMode(goldenDanggnPercent: Int): Boolean {
-        return Random.nextInt(100) > goldenDanggnPercent
+        return Random.nextInt(1, 100) <= goldenDanggnPercent
     }
 }
 

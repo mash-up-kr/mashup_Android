b63d067af78e16196884ae2964a6d8e3a46f2304
jaeryo2357 <jaeryo2357@naver.com>
2023-05-01 01:13:54 +0900

    🛠 #307 DanggnGameController 구현
    - modeController, scoreController를 가지고 있으면서 while 반복문으로 bit rate만큼 돌면서 계속 게임 상황을 체크

--- feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnGameController.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnGameController.kt
@@ -3,43 +3,34 @@ package com.mashup.feature.danggn.data.danggn
 import com.mashup.feature.danggn.data.ShakeDetector
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicInteger
 import javax.inject.Inject
 
 /**
  * ShakeDetector의 shake 이벤트를 탐지하여 콤보 카운트를 계산합니다.
  */
 class DanggnGameController @Inject constructor(
-    private val shakeDetector: ShakeDetector
+    private val shakeDetector: ShakeDetector,
+    private val modeController: DanggnModeController,
+    private val scoreController: DanggnScoreController
 ) {
 
     companion object {
         private const val DEFAULT_FRAME_RATE = 200L
-        private const val COMBO_TERM_DURATION = 2000L
-        private const val TIME_GOLDEN_STAGE = 3000L
     }
-
-    private val danggnModeChannel = Channel<DanggnMode>()
-    private val shakerStateChannel = Channel<DanggnShakerState>()
-    private val comboScoreChannel = Channel<Int>()
 
     private var danggnShakerScope: CoroutineScope? = null
 
-    private var comboScore = AtomicInteger()
+    private var frameCallbackListener: ((DanggnGameState) -> Unit)? = null
 
-    private var danggnMode: DanggnMode = NormalDanggnMode
-
-    fun getDanggnShakeState(): Flow<DanggnShakerState> = shakerStateChannel.consumeAsFlow()
+    /**
+     * DanggnComboCount 넘겨주는 Listener
+     * @param comboCount 누적된 콤보 카운트
+     */
+    private var comboEndCallbackListener: ((comboCount: Int) -> Unit)? = null
 
     fun start(
         threshold: Int,
@@ -50,27 +41,27 @@ class DanggnGameController @Inject constructor(
         shakeDetector.startListening(
             threshold = threshold,
             interval = interval,
-            onShakeDevice = {
-                danggnShakerScope?.launch {
-                    val newComboScore = comboScore.updateAndGet(danggnMode::getNextScore)
-                    shakerStateChannel.send(
-                        DanggnShakerState.Combo(
-                            mode = danggnMode,
-                            score = newComboScore
-                        )
-                    )
-                    comboScoreChannel.send(newComboScore)
-                }
-            }
+            onShakeDevice = this::onShakeDevice
         )
+        modeController.setGoldDanggnPercent(goldenDanggnPercent)
         runDanggnGame()
     }
 
-    fun runDanggnGame() {
+    private fun runDanggnGame() {
         danggnShakerScope?.launch {
             while (danggnShakerScope?.isActive == null) {
+                modeController.checkDanggnMode()
+                scoreController.checkRemainDanggnScore()
 
-
+                val lastComboScore = scoreController.getLastCombonScore()
+                if (lastComboScore > 0) {
+                    comboEndCallbackListener?.invoke(lastComboScore)
+                }
+                frameCallbackListener?.invoke(
+                    DanggnGameState(
+                        danggnScoreModelList = scoreController.getDanggnScoreList()
+                    )
+                )
                 delay(DEFAULT_FRAME_RATE)
             }
         }
@@ -79,54 +70,14 @@ class DanggnGameController @Inject constructor(
     fun stop() {
         danggnShakerScope?.cancel()
         shakeDetector.stopListening()
-        clearFlag()
+        modeController.reset()
+        scoreController.reset()
     }
 
-//    private fun collectComboFlow() {
-//        danggnShakerScope?.launch {
-//            comboScoreChannel.consumeAsFlow().debounce(COMBO_TERM_DURATION)
-//                .collectLatest {
-//                    val lastScore = comboScore.getAndSet(0)
-//                    shakerStateChannel.send(
-//                        DanggnShakerState.End(
-//                            mode = danggnMode,
-//                            lastScore = lastScore
-//                        )
-//                    )
-//                    shakerStateChannel.send(DanggnShakerState.Idle(mode = danggnMode))
-//                }
-//        }
-//    }
-//
-//    private fun runComboScoreController(goldenDanggnPercent: Int) {
-//        danggnShakerScope?.launch {
-//            while (danggnShakerScope?.isActive == true) {
-//                comboScoreChannel.receive()
-//                if (
-//                    (danggnMode as? NormalDanggnMode)?.canSwitchToGoldenDanggnMode(
-//                        goldenDanggnPercent
-//                    ) == true
-//                ) {
-//                    danggnModeChannel.send(GoldenDanggnMode)
-//                }
-//            }
-//        }
-//    }
-//
-//    private fun runDanggnModeController() {
-//        danggnShakerScope?.launch {
-//            while (danggnShakerScope?.isActive == true) {
-//                danggnMode = danggnModeChannel.receiveMode()
-//
-//                if (danggnMode is GoldenDanggnMode) {
-//                    delay(TIME_GOLDEN_STAGE)
-//                    danggnModeChannel.send(NormalDanggnMode)
-//                }
-//            }
-//        }
-//    }
+    private fun onShakeDevice() {
+        modeController.switchToGoldenDanggnMode()
 
-    private fun clearFlag() {
-        comboScore.set(0)
+        val mode = modeController.getDanggnMode()
+        scoreController.addScore(mode)
     }
 }
---
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/danggn/DanggnGameState.kt
@@ -0,0 +1,5 @@
+package com.mashup.feature.danggn.data.danggn
+
+data class DanggnGameState(
+    val danggnScoreModelList: List<DanggnScoreModel>
+)

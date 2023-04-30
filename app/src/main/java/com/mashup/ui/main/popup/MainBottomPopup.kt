31bb291301cf394d8c2373b9ed3f4ae37c68af03
jaeryo2357 <jaeryo2357@naver.com>
2023-05-01 03:54:15 +0900

    🧸 #310 Storage ViewModel 연결

--- app/src/main/java/com/mashup/ui/main/popup/MainBottomPopup.kt
+++ app/src/main/java/com/mashup/ui/main/popup/MainBottomPopup.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -15,26 +14,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mashup.core.ui.colors.White
 import com.mashup.core.ui.typography.SubTitle1
 import com.mashup.core.ui.widget.ButtonStyle
 import com.mashup.core.ui.widget.MashUpButton
+import com.mashup.ui.main.model.MainPopupEntity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainBottomPopup : BottomSheetDialogFragment() {
+
+    private val viewModel: MainBottomPopupViewModel by viewModels()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return super.onCreateDialog(savedInstanceState).apply {
@@ -49,30 +56,45 @@ class MainBottomPopup : BottomSheetDialogFragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MainBottomPopupScreen(
+                    viewModel = viewModel,
+                    onClickLeftButton = {
+                        dismiss()
+                    },
+                    onClickRightButton = {
+                        dismiss()
+                    }
+                )
+            }
         }
     }
 }
 
 @Composable
 fun MainBottomPopupScreen(
+    viewModel: MainBottomPopupViewModel,
     onClickLeftButton: () -> Unit = {},
     onClickRightButton: () -> Unit = {}
 ) {
-    MainBottomPopupContent(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-    )
+    val uiState: MainBottomPopupUiState by viewModel.uiState
+
+    (uiState as? MainBottomPopupUiState.Success)?.let {
+        MainBottomPopupContent(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            mainPopupEntity = it.mainPopupEntity,
+            onClickLeftButton = onClickLeftButton,
+            onClickRightButton = onClickRightButton
+        )
+    }
 }
 
 
 @Composable
 fun MainBottomPopupContent(
-    title: String,
-    description: String,
-    @DrawableRes imageRes: Int,
-    leftButtonText: String,
-    rightButtonText: String,
+    mainPopupEntity: MainPopupEntity,
     modifier: Modifier = Modifier,
     onClickLeftButton: () -> Unit = {},
     onClickRightButton: () -> Unit = {}
@@ -85,7 +107,8 @@ fun MainBottomPopupContent(
             )
     ) {
         Divider(
-            modifier = Modifier.width(24.dp)
+            modifier = Modifier
+                .width(24.dp)
                 .padding(vertical = 10.dp),
             color = Color(0xFFD9D9D9),
             thickness = 1.dp
@@ -96,7 +119,7 @@ fun MainBottomPopupContent(
                 .fillMaxWidth()
                 .padding(top = 8.dp)
                 .padding(horizontal = 20.dp),
-            text = title,
+            text = mainPopupEntity.title,
             style = SubTitle1,
             textAlign = TextAlign.Center
         )
@@ -105,16 +128,24 @@ fun MainBottomPopupContent(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 8.dp),
-            text = description,
+            text = mainPopupEntity.description,
             style = SubTitle1,
             textAlign = TextAlign.Center
         )
 
+        val context = LocalContext.current
+        val imageResId = remember(mainPopupEntity.imageResName) {
+            context.resources.getIdentifier(
+                mainPopupEntity.imageResName,
+                "drawable",
+                context.packageName
+            )
+        }
         Image(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 16.dp),
-            painter = painterResource(id = imageRes),
+            painter = painterResource(id = imageResId),
             contentDescription = null
         )
 
@@ -126,13 +157,13 @@ fun MainBottomPopupContent(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             MashUpButton(
-                text = leftButtonText,
+                text = mainPopupEntity.leftButtonText,
                 buttonStyle = ButtonStyle.INVERSE,
                 onClick = onClickLeftButton
             )
 
             MashUpButton(
-                text = rightButtonText,
+                text = mainPopupEntity.rightButtonText,
                 buttonStyle = ButtonStyle.INVERSE,
                 onClick = onClickRightButton
             )

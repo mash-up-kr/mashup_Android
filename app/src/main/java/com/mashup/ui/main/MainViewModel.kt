ed6dd5f74546d92ddc5548f2c4614d27a09e0c08
jaeryo2357 <jaeryo2357@naver.com>
2023-05-02 01:34:47 +0900

    🧸 #316 DANGGN 비활성화하는 예외처리

--- app/src/main/java/com/mashup/ui/main/MainViewModel.kt
+++ app/src/main/java/com/mashup/ui/main/MainViewModel.kt
@@ -108,6 +108,7 @@ class MainViewModel @Inject constructor(
     }
 
     fun disablePopup(popupKey: MainPopupType) = mashUpScope {
+        if (popupKey == MainPopupType.UNKNOWN) return@mashUpScope
         popUpRepository.patchPopupDisabled(popupKey.name)
     }
 

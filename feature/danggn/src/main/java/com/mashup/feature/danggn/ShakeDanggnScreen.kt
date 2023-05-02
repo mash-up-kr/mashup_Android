9c57b9c39ea34a91bfba67e981e12d70199a272f
정민지 <s2018s38@e-mirim.hs.kr>
2023-05-02 22:25:28 +0900

    🧸 Shake Effect 영역 선언 및 황금 당근이 등장했을 때 배경 딤 처리

--- feature/danggn/src/main/java/com/mashup/feature/danggn/ShakeDanggnScreen.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/ShakeDanggnScreen.kt
@@ -1,16 +1,21 @@
 package com.mashup.feature.danggn
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.mashup.core.ui.colors.Gray100
 import com.mashup.core.ui.widget.MashUpToolbar
+import com.mashup.feature.danggn.data.danggn.GoldenDanggnMode
 import com.mashup.feature.danggn.ranking.DanggnRankingContent
 import com.mashup.feature.danggn.ranking.DanggnRankingViewModel
 import com.mashup.feature.danggn.shake.DanggnShakeContent
@@ -25,6 +30,7 @@ fun ShakeDanggnScreen(
     onClickDanggnInfoButton: () -> Unit,
 ) {
     val uiState by viewModel.uiState.collectAsState(DanggnUiState.Loading)
+    val danggnMode by viewModel.danggnMode.collectAsState()
     val uiRankState by rankingViewModel.mashUpRankingList.collectAsState()
     val personalRankState by rankingViewModel.personalRanking.collectAsState()
 
@@ -32,32 +38,64 @@ fun ShakeDanggnScreen(
         viewModel.startDanggnGame()
     }
 
-    Column(
-        modifier = modifier
-    ) {
-        MashUpToolbar(
-            title = "당근 흔들기",
-            showBackButton = true,
-            onClickBackButton = onClickBackButton,
-            showActionButton = true,
-            onClickActionButton = onClickDanggnInfoButton,
-            actionButtonDrawableRes = CR.drawable.ic_info
-        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = modifier
+        ) {
+            MashUpToolbar(
+                title = "당근 흔들기",
+                showBackButton = true,
+                onClickBackButton = onClickBackButton,
+                showActionButton = true,
+                onClickActionButton = onClickDanggnInfoButton,
+                actionButtonDrawableRes = CR.drawable.ic_info
+            )
 
-        // 당근 흔들기 UI
-        DanggnShakeContent(viewModel = viewModel)
+            // 당근 흔들기 UI
+            DanggnShakeContent(viewModel = viewModel)
 
-        // 중간 Divider
-        Divider(
-            color = Gray100,
-            modifier = Modifier.fillMaxWidth(),
-            thickness = 4.dp
-        )
+            // 중간 Divider
+            Divider(
+                color = Gray100,
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 4.dp
+            )
 
-        // 당근 흔들기 랭킹 UI
-        DanggnRankingContent(
-            allRankList = uiRankState.sortedByDescending { it.totalShakeScore },
-            personalRank = personalRankState
-        )
+            // 당근 흔들기 랭킹 UI
+            DanggnRankingContent(
+                allRankList = uiRankState.sortedByDescending { it.totalShakeScore },
+                personalRank = personalRankState
+            )
+        }
+
+        // Shake Effect 영역
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(if (danggnMode is GoldenDanggnMode) Color(0xCC000000) else Color.Transparent)
+        ) {
+            if (danggnMode is GoldenDanggnMode) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 50.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = CR.drawable.img_fevertime_title),
+                        contentDescription = null,
+                        modifier = Modifier.width(300.dp)
+                    )
+                }
+
+                Image(
+                    painter = painterResource(id = CR.drawable.img_fever_danggn),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(174.dp)
+                        .align(Alignment.Center)
+                )
+            }
+        }
     }
 }

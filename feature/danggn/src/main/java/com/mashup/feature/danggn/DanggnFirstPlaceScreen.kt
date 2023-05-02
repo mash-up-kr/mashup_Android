be3b1ff4cba07352c8e5d8379eafdf86af81c23a
Ahn-seokjoo <tjrwn0716@naver.com>
2023-05-03 01:44:56 +0900

    [feature] 1등 알리미 ui 추가

---
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/DanggnFirstPlaceScreen.kt
@@ -0,0 +1,98 @@
+package com.mashup.feature.danggn
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.core.ui.theme.MashUpTheme
+import com.mashup.core.ui.typography.Title1
+import com.mashup.core.ui.widget.MashUpButton
+
+@Composable
+fun DanggnFirstPlaceScreen(
+    modifier: Modifier = Modifier,
+    userName: String = "매숑이",
+    onClickCloseButton: (() -> Unit)? = null,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        DanggnFirstPlaceContent(modifier, userName)
+        Image(
+            modifier = Modifier
+                .padding(top = 70.dp, end = 26.dp)
+                .align(Alignment.TopEnd)
+                .clickable {
+                    onClickCloseButton?.invoke()
+                },
+            alignment = Alignment.CenterEnd,
+            painter = painterResource(id = com.mashup.core.common.R.drawable.ic_close),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(Color.White)
+        )
+    }
+}
+
+@Composable
+private fun DanggnFirstPlaceContent(modifier: Modifier, userName: String) {
+    Column(
+        modifier = modifier
+            .background(color = Color(0xB3000000))
+            .fillMaxSize()
+            .padding(horizontal = 38.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier,
+            painter = painterResource(id = R.drawable.img_carrot_rank_in),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Text(
+            modifier = Modifier,
+            text = "축하드려요!\n${userName}님이 1등을 차지했어요!",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            style = Title1
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        MashUpButton(
+            modifier = Modifier
+                .width(120.dp)
+                .height(52.dp),
+            text = "확인", onClick = { /*TODO*/ })
+    }
+}
+
+@Composable
+@Preview
+fun PreviewDanggnFirstPlace() {
+    MashUpTheme {
+        Surface(color = Color.White) {
+            DanggnFirstPlaceScreen()
+        }
+    }
+}

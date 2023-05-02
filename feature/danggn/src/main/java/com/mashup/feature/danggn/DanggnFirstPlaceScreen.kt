1bdc730cd3e83d7c9ce5dc35ad420ec4d7ad9787
Ahn-seokjoo <tjrwn0716@naver.com>
2023-05-03 01:50:06 +0900

    [refactoring] 팀이름 수정

--- feature/danggn/src/main/java/com/mashup/feature/danggn/DanggnFirstPlaceScreen.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/DanggnFirstPlaceScreen.kt
@@ -26,16 +26,22 @@ import com.mashup.core.ui.theme.MashUpTheme
 import com.mashup.core.ui.typography.Title1
 import com.mashup.core.ui.widget.MashUpButton
 
+/**
+ * 개인일 경우 {username}님
+ * 플랫폼일 경우 {platforrm}팀
+ * 까지 받아야합니다!!!!!!!! 나머지 텍스트가 공통입니다
+ * [figma] https://www.figma.com/file/kxgTs6r19oJz6ipQGYm83d/%EB%A7%A4%EC%89%AC%EC%97%85-%EC%95%B1?node-id=504-29106&t=ExMNilr25jrPhyWt-0
+ */
 @Composable
 fun DanggnFirstPlaceScreen(
     modifier: Modifier = Modifier,
-    userName: String = "매숑이",
+    name: String = "매숑이님",
     onClickCloseButton: (() -> Unit)? = null,
 ) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        DanggnFirstPlaceContent(modifier, userName)
+        DanggnFirstPlaceContent(modifier, name)
         Image(
             modifier = Modifier
                 .padding(top = 70.dp, end = 26.dp)
@@ -52,7 +58,7 @@ fun DanggnFirstPlaceScreen(
 }
 
 @Composable
-private fun DanggnFirstPlaceContent(modifier: Modifier, userName: String) {
+private fun DanggnFirstPlaceContent(modifier: Modifier, name: String) {
     Column(
         modifier = modifier
             .background(color = Color(0xB3000000))
@@ -71,7 +77,7 @@ private fun DanggnFirstPlaceContent(modifier: Modifier, userName: String) {
 
         Text(
             modifier = Modifier,
-            text = "축하드려요!\n${userName}님이 1등을 차지했어요!",
+            text = "축하드려요!\n${name}이 1등을 차지했어요!",
             textAlign = TextAlign.Center,
             color = Color.White,
             style = Title1

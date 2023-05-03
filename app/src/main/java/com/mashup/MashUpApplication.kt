fdc4f7a6efe48544f4215bf38ef554aa02d68cd0
Ahn-seokjoo <tjrwn0716@naver.com>
2023-05-04 00:58:59 +0900

    [feature] 플리퍼 추가 (network, layout, logging)

--- app/src/main/java/com/mashup/MashUpApplication.kt
+++ app/src/main/java/com/mashup/MashUpApplication.kt
@@ -1,7 +1,25 @@
 package com.mashup
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MashUpApplication : Application()
+class MashUpApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(NetworkFlipperPlugin())
+            client.start()
+        }
+    }
+}

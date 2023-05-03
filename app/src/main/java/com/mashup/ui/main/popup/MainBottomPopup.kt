adbe884ab5b9e036bba6c252ef346521ef0b4853
jaeryo2357 <jaeryo2357@naver.com>
2023-05-04 01:10:33 +0900

    🛠 #332 viewed api 호출 onCreate에서 하기

--- app/src/main/java/com/mashup/ui/main/popup/MainBottomPopup.kt
+++ app/src/main/java/com/mashup/ui/main/popup/MainBottomPopup.kt
@@ -6,16 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -69,6 +60,11 @@ class MainBottomPopup : BottomSheetDialogFragment() {
 
     private val viewModel: MainBottomPopupViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.patchPopupViewed()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -82,11 +78,9 @@ class MainBottomPopup : BottomSheetDialogFragment() {
                     MainBottomPopupScreen(
                         viewModel = viewModel,
                         onClickLeftButton = {
-                            viewModel.patchPopupViewed()
                             dismiss()
                         },
                         onClickRightButton = {
-                            viewModel.patchPopupViewed()
                             mainViewModel.onClickPopup(viewModel.popupKey ?: "")
                             dismiss()
                         }

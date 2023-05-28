eee5a24e4539b9662b26ad6ba9bcad786c7320bf
jaeryo2357 <jaeryo2357@naver.com>
2023-05-29 05:18:13 +0900

    🐛 #338 mockito kotlin 추가 및 적용

--- app/src/test/java/com/mashup/feature/danggn/ranking/DanggnRankingViewModelTest.kt
+++ app/src/test/java/com/mashup/feature/danggn/ranking/DanggnRankingViewModelTest.kt
@@ -1,15 +1,19 @@
 package com.mashup.feature.danggn.ranking
 
+import com.mashup.core.model.data.local.DanggnPreference
+import com.mashup.core.model.data.local.UserPreference
+import com.mashup.datastore.data.repository.DanggnPreferenceRepository
+import com.mashup.datastore.data.repository.UserPreferenceRepository
 import com.mashup.fake.FakeDanggnDao
-import com.mashup.feature.danggn.data.dto.DanggnMemberRankResponse
 import com.mashup.feature.danggn.data.repository.DanggnRepository
 import com.mashup.util.CoroutineRule
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
-import org.junit.Before
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
 /**
  * 경축
@@ -23,40 +27,47 @@ class DanggnRankingViewModelTest {
 
     private lateinit var rankingViewModel: DanggnRankingViewModel
 
-    @Before
-    fun setUp() {
-        rankingViewModel = DanggnRankingViewModel(
-            DanggnRepository(fakeRankingDao)
-        )
+    private val defaultUserPreferenceRepository = mock<UserPreferenceRepository> {
+        on { getUserPreference() } doReturn flowOf(UserPreference.getDefaultInstance())
+    }
+
+    private val defaultDanggnPreferenceRepository = mock<DanggnPreferenceRepository> {
+        on { getDanggnPreference() } doReturn flowOf(DanggnPreference.getDefaultInstance())
     }
 
     @Test
-    fun `데이터가 잘들어오는지에 대한 테스트`() {
+    fun `DanggnMemberRankResponse 올바르게 PlatformRanking으로 변환되는지 테스트`() {
         // given
+        rankingViewModel = DanggnRankingViewModel(
+            danggnRepository = DanggnRepository(fakeRankingDao),
+            userPreferenceRepository = defaultUserPreferenceRepository,
+            danggnPreferenceRepository = defaultDanggnPreferenceRepository
+        )
 
-        coroutineRule.testDispatcher.scheduler.runCurrent()
         // when
+        coroutineRule.testDispatcher.scheduler.advanceTimeBy(3000L)
+
         val list = listOf(
-            DanggnMemberRankResponse(
-                39, "정종노드", 150
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "39", "정종노드", 150
             ),
-            DanggnMemberRankResponse(
-                40, "정종드투", 151
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "40", "정종드투", 151
             ),
-            DanggnMemberRankResponse(
-                41, "정종민", 152
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "41", "정종민", 152
             ),
-            DanggnMemberRankResponse(
-                42, "정종웹", 153
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "42", "정종웹", 153
             ),
-            DanggnMemberRankResponse(
-                43, "정종오스", 154
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "43", "정종오스", 154
             ),
-            DanggnMemberRankResponse(
-                44, "정종자인", 155
+            DanggnRankingViewModel.RankingItem.PlatformRanking(
+                "44", "정종자인", 155
             ),
         )
         // then
-        assertEquals(rankingViewModel.mashUpRankingList.value, list)
+        assertEquals(rankingViewModel.uiState.value.personalRankingList, list)
     }
 }

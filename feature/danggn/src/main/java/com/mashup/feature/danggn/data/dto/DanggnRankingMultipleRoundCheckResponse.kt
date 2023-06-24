b149674978aa5faaf9a1d0b3336c38b4673b57b0
Ahn-seokjoo <tjrwn0716@naver.com>
2023-06-24 23:58:52 +0900

    [feat] 네이밍 변경 및 dao 추가

--- feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnRankingMultipleRoundCheckResponse.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnRankingMultipleRoundCheckResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class DanggnRankingMultipleTimeCheckResponse(
+data class DanggnRankingMultipleRoundCheckResponse(
     @Json(name = "danggnRankingRounds")
     val danggnRankingRounds: List<DanggnRankingRound>
 ) {

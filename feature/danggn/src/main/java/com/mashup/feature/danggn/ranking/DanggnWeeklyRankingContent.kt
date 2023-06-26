6dcc7ddaca6d9e9f8b0c79ee6fd7d7ad7faf7b8e
Ahn-seokjoo <tjrwn0716@naver.com>
2023-06-26 23:51:02 +0900

    [feat] 미사용 import 제거

--- feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnWeeklyRankingContent.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnWeeklyRankingContent.kt
@@ -1,6 +1,5 @@
 package com.mashup.feature.danggn.ranking
 
-import android.graphics.Paint.Cap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString

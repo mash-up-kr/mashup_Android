28f6198c0c277a56978562f75e7ad6587fda6cb3
jaeryo2357 <jaeryo2357@naver.com>
2023-06-30 23:40:18 +0900

    🛠 DanggnRankingRewardState 주석 위치 수정

--- feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnRankingSingleRoundCheckResponse.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/data/dto/DanggnRankingSingleRoundCheckResponse.kt
@@ -27,10 +27,10 @@ data class DanggnRankingSingleRoundCheckResponse(
         val status: String
     ) {
         enum class DanggnRankingRewardStatus {
-            FIRST_PLACE_MEMBER_NOT_EXISTED,        // 1등한 사람이 없는 경우(1회차)
-            FIRST_PLACE_MEMBER_NOT_REGISTERED,    // 회차는 진행되었지만, 1등한 사람이 리워드를 등록하지 않은 경우
-            FIRST_PLACE_MEMBER_REGISTERED,
-            UNKNOWN;      // 회차가 진행되었고 1등한 사람이 리워드를 등록한 경우
+            FIRST_PLACE_MEMBER_NOT_EXISTED, // 1등한 사람이 없는 경우(1회차)
+            FIRST_PLACE_MEMBER_NOT_REGISTERED, // 회차는 진행되었지만, 1등한 사람이 리워드를 등록하지 않은 경우
+            FIRST_PLACE_MEMBER_REGISTERED, // 회차가 진행되었고 1등한 사람이 리워드를 등록한 경우
+            UNKNOWN;
 
             companion object {
                 fun getDanggnRankingRewardStatus(status: String) = values().find { it.name == status } ?: UNKNOWN

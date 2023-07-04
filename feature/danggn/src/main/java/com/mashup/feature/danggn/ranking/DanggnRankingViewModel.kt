f33cbba821d3c3e0cb3b7c8ed29754da365a854c
정민지 <s2018s38@e-mirim.hs.kr>
2023-07-04 20:17:02 +0900

    🛠 runCatching 및 mashUpScope 사용하도록 수정

--- feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnRankingViewModel.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/ranking/DanggnRankingViewModel.kt
@@ -19,7 +19,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.UUID
@@ -293,13 +292,11 @@ class DanggnRankingViewModel @Inject constructor(
     }
 
     fun registerRewardNotice(round: Int, comment: String) {
-        viewModelScope.launch {
-            try {
-                danggnRepository.postDanggnRankingRewardComment(round, comment).also {
-                    // TODO: 랭킹 단건조회 & 리프레쉬
-                }
-            } catch (e: Exception) {
-
+        mashUpScope {
+            kotlin.runCatching {
+                danggnRepository.postDanggnRankingRewardComment(round, comment)
+            }.onSuccess {
+                // TODO: 랭킹 단건조회 & 리프레쉬
             }
         }
     }

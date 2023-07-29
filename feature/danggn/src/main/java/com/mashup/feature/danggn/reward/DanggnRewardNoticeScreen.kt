d73ba2868e04fcbae0dac4f274a962650d6105c4
jaeryo2357 <jaeryo2357@naver.com>
2023-07-30 03:37:29 +0900

    🐛 당근 나머지 오류 수정

--- feature/danggn/src/main/java/com/mashup/feature/danggn/reward/DanggnRewardNoticeScreen.kt
+++ feature/danggn/src/main/java/com/mashup/feature/danggn/reward/DanggnRewardNoticeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -56,14 +57,15 @@ private fun DanggnRewardNoticeContent(
 ) {
     Column(
         modifier = modifier
-            .background(color = Color(0xB3000000))
-            .padding(horizontal = 48.dp),
+            .background(color = Color(0xB3000000)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
         Column(
             modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 48.dp)
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(23.dp)

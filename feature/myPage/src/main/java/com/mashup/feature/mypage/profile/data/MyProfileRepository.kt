7cd467341a3d274d3d00c667f2c3d40608901ddc
Ahn-seokjoo <tjrwn0716@naver.com>
2023-08-25 18:23:45 +0900

    🧸 API 명세 작성

---
+++ feature/myPage/src/main/java/com/mashup/feature/mypage/profile/data/MyProfileRepository.kt
@@ -0,0 +1,28 @@
+package com.mashup.feature.mypage.profile.data
+
+import com.mashup.feature.mypage.profile.data.dto.MemberProfileRequest
+import com.mashup.feature.mypage.profile.edit.EditedProfile
+import javax.inject.Inject
+
+class MyProfileRepository @Inject constructor(
+    private val myProfileDao: MyProfileDao
+) {
+    suspend fun getMyProfile() = myProfileDao.getMemberProfile()
+
+    suspend fun postMyProfile(
+        editedProfile: EditedProfile
+    ) = myProfileDao.postMemberProfile(
+        MemberProfileRequest(
+            birthDate = editedProfile.birthDay, // 생년월일
+            blogLink = editedProfile.tistory, // 티스토리
+            company = editedProfile.company, // 회사명
+            githubLink = editedProfile.github, //  깃헙
+            introduction = editedProfile.introduceMySelf, // 자기소개
+            job = editedProfile.work, // 직군
+            linkedInLink = editedProfile.linkedIn, // 링크드인
+            portfolioLink = editedProfile.behance, // 포트폴리오
+            residence = editedProfile.location, // 출몰지역
+            socialNetworkServiceLink = editedProfile.instagram, // sns
+        )
+    )
+}

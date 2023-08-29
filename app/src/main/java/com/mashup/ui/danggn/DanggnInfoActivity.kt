aa98fcf3f7a76a49deba40a337664eec751f32bc
Ahn-seokjoo <tjrwn0716@naver.com>
2023-08-30 00:17:45 +0900

    🧸 clean code

--- app/src/main/java/com/mashup/ui/danggn/DanggnInfoActivity.kt
+++ app/src/main/java/com/mashup/ui/danggn/DanggnInfoActivity.kt
@@ -3,17 +3,13 @@ package com.mashup.ui.danggn
 import android.content.Context
 import android.content.Intent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import com.mashup.R
 import com.mashup.base.BaseActivity
 import com.mashup.constant.EXTRA_ANIMATION
 import com.mashup.core.common.model.NavigationAnimationType
 import com.mashup.core.ui.theme.MashUpTheme
 import com.mashup.databinding.ActivityDanggnInfoBinding
-import com.mashup.feature.danggn.DanggnInfoScreen
 import com.mashup.feature.mypage.profile.MyPageProfileEditViewModel
-import com.mashup.feature.mypage.profile.edit.MyPageEditCardScreen
 import com.mashup.feature.mypage.profile.edit.MyPageEditProfileScreen
 import dagger.hilt.android.AndroidEntryPoint
 

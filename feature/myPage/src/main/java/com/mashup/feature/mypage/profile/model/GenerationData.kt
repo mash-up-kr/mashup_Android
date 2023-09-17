7f3e92c023512753260b7701a931693e3a332df1
정민지 <s2018s38@e-mirim.hs.kr>
2023-09-17 21:05:04 +0900

    🧸 활동카드 HorizontalPager UI 개발

--- feature/myPage/src/main/java/com/mashup/feature/mypage/profile/model/GenerationData.kt
+++ feature/myPage/src/main/java/com/mashup/feature/mypage/profile/model/GenerationData.kt
@@ -2,6 +2,8 @@ package com.mashup.feature.mypage.profile.model
 
 data class GenerationData(
     val id: Int = 0,
+    val name: String,
+    val isRunning: Boolean,
     val generationNumber: Int = 0,
     val platform: String = "",
     val projectTeamName: String = "",

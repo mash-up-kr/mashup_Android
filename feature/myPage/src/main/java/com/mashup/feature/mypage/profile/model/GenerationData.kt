d9a619ae46d227a656d65687ccb51f6548ccc943
정민지 <s2018s38@e-mirim.hs.kr>
2023-09-17 23:29:24 +0900

    🧸 Generation 정보 가져와서 마이페이지 활동카드 보여줄 AttendanceModel에 넣어주기

---
+++ feature/myPage/src/main/java/com/mashup/feature/mypage/profile/model/GenerationData.kt
@@ -0,0 +1,9 @@
+package com.mashup.feature.mypage.profile.model
+
+data class GenerationData(
+    val id: Int = 0,
+    val generationNumber: Int = 0,
+    val platform: String = "",
+    val projectTeamName: String = "",
+    val role: String = "",
+)

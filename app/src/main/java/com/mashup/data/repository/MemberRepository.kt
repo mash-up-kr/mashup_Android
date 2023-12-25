8ac7fcea148746d4832ac887b1bf9a4302283b10
boris-xyz <62296097+014967@users.noreply.github.com>
2023-12-25 20:52:50 +0900

    비밀번호 변경 API 생성

--- app/src/main/java/com/mashup/data/repository/MemberRepository.kt
+++ app/src/main/java/com/mashup/data/repository/MemberRepository.kt
@@ -8,6 +8,7 @@ import com.mashup.data.dto.MemberInfoResponse
 import com.mashup.data.dto.PushNotificationRequest
 import com.mashup.data.dto.SignUpRequest
 import com.mashup.data.dto.TokenResponse
+import com.mashup.data.dto.UpdatePasswordRequest
 import com.mashup.data.dto.ValidResponse
 import com.mashup.network.dao.MemberDao
 import javax.inject.Inject
@@ -91,5 +92,13 @@ class MemberRepository @Inject constructor(
         )
 
         return memberDao.patchPushNotification(requestBody)
+    }
+
+    suspend fun updatePassword(
+        id: String,
+        pwd: String,
+    ): Response<Any> {
+        val requestBody = UpdatePasswordRequest(newPassword = pwd)
+        return memberDao.updatePassword(id = id, request = requestBody)
     }
 }

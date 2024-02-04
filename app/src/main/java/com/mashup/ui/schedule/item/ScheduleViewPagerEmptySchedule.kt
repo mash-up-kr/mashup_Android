9eb933b14b1730a7affa80f075fe4592d7f87ac2
boris-xyz <62296097+014967@users.noreply.github.com>
2024-02-04 18:55:21 +0900

    스케줄이 비어있을 때 UI 구현

---
+++ app/src/main/java/com/mashup/ui/schedule/item/ScheduleViewPagerEmptySchedule.kt
@@ -0,0 +1,98 @@
+package com.mashup.ui.schedule.item
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.R
+import com.mashup.core.ui.colors.Gray100
+import com.mashup.core.ui.colors.Gray400
+import com.mashup.core.ui.colors.Gray50
+import com.mashup.core.ui.theme.MashUpTheme
+import com.mashup.core.ui.typography.Body1
+import com.mashup.ui.schedule.model.ScheduleCard
+
+@Composable
+fun ScheduleViewPagerEmptyItem(
+    data: ScheduleCard.EmptySchedule,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth().wrapContentHeight().background(
+            color = Color.White,
+            shape = RoundedCornerShape(20.dp)
+        ).border(
+            width = 1.dp,
+            color = Gray100,
+            shape = RoundedCornerShape(20.dp)
+        ).padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CardInfoItem(
+            modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+            dDay = data.scheduleResponse?.getDDay() ?: "?",
+            title = data.scheduleResponse?.name ?: "",
+            calendar = data.scheduleResponse?.getDate() ?: "-",
+            timeLine = data.scheduleResponse?.getTimeLine() ?: "-",
+            location = ""
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(
+            modifier = Modifier.fillMaxWidth().background(
+                color = Gray50,
+                shape = RoundedCornerShape(16.dp)
+            ).padding(
+                vertical = 22.dp,
+                horizontal = 20.dp
+            ),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.padding(vertical = 66.dp, horizontal = 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(
+                        com.mashup.core.common.R.drawable.img_placeholder_sleeping
+                    ),
+                    contentDescription = null
+                )
+                Spacer(
+                    modifier = Modifier.height(10.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.description_empty_schedule),
+                    style = Body1,
+                    color = Gray400,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewEmptySchedule() {
+    MashUpTheme {
+        ScheduleViewPagerEmptyItem(data = ScheduleCard.EmptySchedule())
+    }
+}

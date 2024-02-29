3b370373672378635840cb2f2f56ea6e69e2d7d1
정민지 <21200203@mobmail.shinhan.com>
2024-02-29 16:35:56 +0900

    🧸 세미나 타이틀 및 세부 정보(Info) 보여주는 ViewHolder 추가
    - 타이틀
    - 날짜
    - 시간

--- app/src/main/java/com/mashup/ui/schedule/detail/ScheduleDetailViewModel.kt
+++ app/src/main/java/com/mashup/ui/schedule/detail/ScheduleDetailViewModel.kt
@@ -3,6 +3,7 @@ package com.mashup.ui.schedule.detail
 import androidx.lifecycle.SavedStateHandle
 import com.mashup.constant.EXTRA_SCHEDULE_ID
 import com.mashup.core.common.base.BaseViewModel
+import com.mashup.core.common.extensions.getTimeFormat
 import com.mashup.data.dto.ContentResponse
 import com.mashup.data.dto.EventResponse
 import com.mashup.data.dto.ScheduleResponse
@@ -11,6 +12,7 @@ import com.mashup.ui.schedule.model.Body
 import com.mashup.ui.schedule.model.EventDetail
 import com.mashup.ui.schedule.model.EventDetailType
 import com.mashup.ui.schedule.model.Header
+import com.mashup.ui.schedule.model.Info
 import com.mashup.ui.schedule.model.Location
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -41,7 +43,12 @@ class ScheduleDetailViewModel @Inject constructor(
                 .onSuccess { response ->
                     _scheduleState.emit(
                         ScheduleState.Success(
-                            getEventDetailList(response.eventList, response.location)
+                            getEventDetailList(
+                                title = response.name,
+                                date = response.getDate(),
+                                eventList = response.eventList,
+                                location = response.location
+                            )
                         )
                     )
                 }
@@ -58,11 +65,17 @@ class ScheduleDetailViewModel @Inject constructor(
     }
 
     private fun getEventDetailList(
+        title: String,
+        date: String,
         eventList: List<EventResponse>,
-        location: ScheduleResponse.Location?
+        location: ScheduleResponse.Location?,
     ): List<EventDetail> {
         var itemId = 0
         val eventDetailList = mutableListOf<EventDetail>()
+
+        val startAt = eventList.first().startedAt.getTimeFormat()
+        val endedAt = eventList.last().endedAt.getTimeFormat()
+        eventDetailList.add(mapToInfoModel(itemId++, title, date, "$startAt - $endedAt"))
 
         if (location?.placeName != null) { // 위치 정보가 있는 경우(온라인이면 placeName이 Zoom으로 내려옴)
             eventDetailList.add(mapToLocationModel(itemId++, location))
@@ -88,8 +101,6 @@ class ScheduleDetailViewModel @Inject constructor(
                 startedAt = event.startedAt,
                 endedAt = event.endedAt
             ),
-            body = null,
-            location = null
         )
     }
 
@@ -101,14 +112,12 @@ class ScheduleDetailViewModel @Inject constructor(
         return EventDetail(
             id = itemId,
             type = EventDetailType.CONTENT,
-            header = null,
             body = Body(
                 contentId = "${contentIndex + 1}",
                 title = content.title,
                 content = content.content.orEmpty(),
                 startedAt = content.startedAt
             ),
-            location = null
         )
     }
 
@@ -116,14 +125,29 @@ class ScheduleDetailViewModel @Inject constructor(
         return EventDetail(
             id = itemId,
             type = EventDetailType.LOCATION,
-            header = null,
-            body = null,
             location = Location(
                 placeName = location.placeName.orEmpty(),
                 address = location.address.orEmpty(),
                 latitude = location.latitude,
                 longitude = location.longitude
             )
+        )
+    }
+
+    private fun mapToInfoModel(
+        itemId: Int,
+        title: String,
+        date: String,
+        time: String
+    ): EventDetail {
+        return EventDetail(
+            id = itemId,
+            type = EventDetailType.INFO,
+            info = Info(
+                title = title,
+                date = date,
+                time = time,
+            ),
         )
     }
 }

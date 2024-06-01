9dca26b3a8c14d494aeb4455499bf62ab0f18356
정민지 <s2018s38@e-mirim.hs.kr>
2024-06-01 17:20:23 +0900

    🛠 사용하지 않는 import 제거

--- app/src/main/java/com/mashup/ui/schedule/detail/composable/ScheduleDetailLocationContent.kt
+++ app/src/main/java/com/mashup/ui/schedule/detail/composable/ScheduleDetailLocationContent.kt
@@ -1,18 +1,14 @@
 package com.mashup.ui.schedule.detail.composable
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -20,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp

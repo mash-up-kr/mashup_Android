f6baf251b25fe7f43c1e5c3471828de58a253282
014967 <boris@xyzcorp.io>
2024-08-18 13:02:28 +0900

    WEB HOST 추가

--- app/src/release/java/com/mashup/data/network/NetworkConstanst.kt
+++ app/src/release/java/com/mashup/data/network/NetworkConstanst.kt
@@ -1,3 +1,4 @@
 package com.mashup.data.network
 
-const val API_HOST = "https://api.member.mash-up.kr"
+const val API_HOST = "https://api.member.mash-up.kr/"
+const val WEB_HOST = "https://app.mash-up.kr/"

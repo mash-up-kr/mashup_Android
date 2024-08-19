89f3bb63e3f2685f6801c0a255e1a599e8c39b31
정민지 <21200203@mobmail.shinhan.com>
2024-08-19 18:19:33 +0900

    🧸 전체 일정 목록에서 이번 주 일정 포커싱 처리
    - 전체 일정 스크롤 상태 유지

--- app/src/main/java/com/mashup/ui/schedule/ScheduleRoute.kt
+++ app/src/main/java/com/mashup/ui/schedule/ScheduleRoute.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
@@ -106,6 +107,9 @@ fun ScheduleRoute(
         }
     }
 
+    val weeklyListState = rememberLazyListState()
+    val dailyListState = rememberLazyListState()
+
     var selectedTabIndex by remember { mutableIntStateOf(0) }
     CompositionLocalProvider(
         LocalOverscrollConfiguration provides null
@@ -115,7 +119,10 @@ fun ScheduleRoute(
                 .pullRefresh(pullRefreshState)
                 .background(color = if (ScheduleType.values()[selectedTabIndex] == ScheduleType.WEEK) Color.White else Gray50)
         ) {
-            LazyColumn(modifier = modifier) {
+            LazyColumn(
+                modifier = modifier,
+                state = if (selectedTabIndex == 0) weeklyListState else dailyListState
+            ) {
                 item {
                     ScheduleTopbar(
                         title,
@@ -147,6 +154,7 @@ fun ScheduleRoute(
                             ScheduleScreen(
                                 modifier = Modifier.fillMaxSize(),
                                 scheduleState = scheduleState,
+                                dailyListState = dailyListState,
                                 onClickScheduleInformation = { context.moveToScheduleInformation(it) },
                                 onClickAttendance = { context.moveToAttendance(it) },
                                 onClickMashongButton = { context.moveToMashong() },
--- app/src/main/java/com/mashup/ui/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/mashup/ui/schedule/ScheduleScreen.kt
@@ -1,6 +1,8 @@
 package com.mashup.ui.schedule
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.mashup.ui.schedule.component.DailySchedule
 import com.mashup.ui.schedule.component.WeeklySchedule
@@ -9,6 +11,7 @@ import com.mashup.ui.schedule.model.ScheduleType
 @Composable
 fun ScheduleScreen(
     scheduleState: ScheduleState,
+    dailyListState: LazyListState,
     modifier: Modifier = Modifier,
     scheduleType: ScheduleType = ScheduleType.WEEK,
     onClickScheduleInformation: (Int) -> Unit = {},
@@ -16,6 +19,14 @@ fun ScheduleScreen(
     onClickMashongButton: () -> Unit = {},
     refreshState: Boolean = false
 ) {
+    LaunchedEffect(scheduleState) {
+        if (scheduleState is ScheduleState.Success) {
+            if (scheduleState.schedulePosition < scheduleState.scheduleList.size) {
+                dailyListState.animateScrollToItem(scheduleState.schedulePosition)
+            }
+        }
+    }
+
     when (scheduleType) {
         ScheduleType.WEEK -> {
             WeeklySchedule(

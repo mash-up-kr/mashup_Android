0ca154083eaebc34644770b2e03677f6fa7d996c
JeonK1 <kan5016@naver.com>
2026-03-25 20:59:04 +0900

    🧸BaseComposeActivity 생성

---
+++ app/src/main/java/com/mashup/base/BaseComposeActivity.kt
@@ -0,0 +1,155 @@
+package com.mashup.base
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.os.bundleOf
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.mashup.constant.EXTRA_ACTIVITY_ENTER_TYPE
+import com.mashup.constant.EXTRA_ANIMATION
+import com.mashup.core.common.constant.BAD_REQUEST
+import com.mashup.core.common.constant.DISCONNECT_NETWORK
+import com.mashup.core.common.constant.INTERNAL_SERVER_ERROR
+import com.mashup.core.common.constant.UNAUTHORIZED
+import com.mashup.core.common.model.NavigationAnimationType
+import com.mashup.core.common.utils.ProgressbarUtil
+import com.mashup.core.common.utils.ToastUtil
+import com.mashup.core.common.widget.CommonDialog
+import com.mashup.network.NetworkStatusState
+import com.mashup.network.data.NetworkStatusDetector
+import com.mashup.ui.error.NetworkDisconnectActivity
+import com.mashup.ui.login.LoginActivity
+import com.mashup.util.AnalyticsManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+open class BaseComposeActivity : ComponentActivity() {
+    private val networkStateDetector: NetworkStatusDetector by lazy {
+        NetworkStatusDetector(
+            context = this,
+            coroutineScope = lifecycleScope
+        )
+    }
+
+    val isConnectedNetwork: Boolean
+        get() = networkStateDetector.hasNetworkConnection()
+
+    private var animationType: NavigationAnimationType? = null
+
+    private var loadingDialog: Dialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        initAnimationType()
+    }
+
+    override fun finish() {
+        super.finish()
+        animationType?.run {
+            overridePendingTransition(
+                0,
+                exitOut
+            )
+        }
+    }
+
+    private fun initAnimationType() {
+        when (intent.getSerializableExtra(EXTRA_ANIMATION)) {
+            NavigationAnimationType.SLIDE -> {
+                animationType = NavigationAnimationType.SLIDE
+            }
+            NavigationAnimationType.PULL -> {
+                animationType = NavigationAnimationType.PULL
+            }
+        }
+        animationType?.run {
+            overridePendingTransition(
+                enterIn,
+                enterOut
+            )
+        }
+    }
+
+    open fun initObserves() {
+        flowLifecycleScope {
+            networkStateDetector.state.collectLatest { networkState ->
+                when (networkState) {
+                    is NetworkStatusState.NetworkConnected -> {
+                        onNetworkConnected()
+                    }
+                    is NetworkStatusState.NetworkDisconnected -> {
+                        onNetworkDisConnected()
+                    }
+                }
+            }
+        }
+    }
+
+    protected fun flowLifecycleScope(
+        state: Lifecycle.State = Lifecycle.State.STARTED,
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(state) {
+                block.invoke(this)
+            }
+        }
+    }
+
+    open fun onNetworkConnected() {
+    }
+
+    open fun onNetworkDisConnected() {
+    }
+
+    open fun handleCommonError(code: String) {
+        when (code) {
+            BAD_REQUEST, INTERNAL_SERVER_ERROR -> {
+                showToast("잠시 후 다시 시도해주세요.")
+            }
+            UNAUTHORIZED -> {
+                CommonDialog(this).apply {
+                    setTitle(text = "주의")
+                    setMessage(text = "인증정보가 초기화되어 재로그인이 필요합니다")
+                    setPositiveButton {
+                        startActivity(
+                            LoginActivity.newIntent(this@BaseComposeActivity)
+                        )
+                        finish()
+                    }
+                    show()
+                }
+            }
+            DISCONNECT_NETWORK -> {
+                startActivity(
+                    NetworkDisconnectActivity.newIntent(this)
+                )
+            }
+        }
+    }
+
+    protected fun showToast(text: String) {
+        ToastUtil.showToast(this, text)
+    }
+
+    fun showLoading() {
+        if (loadingDialog == null) {
+            loadingDialog = ProgressbarUtil.show(this)
+        }
+    }
+
+    fun hideLoading() {
+        loadingDialog?.dismiss()
+    }
+
+    protected fun sendActivityEnterType(screenName: String) {
+        val type = intent.getStringExtra(EXTRA_ACTIVITY_ENTER_TYPE) ?: return
+        AnalyticsManager.addEvent(screenName, bundleOf("type" to type))
+    }
+}

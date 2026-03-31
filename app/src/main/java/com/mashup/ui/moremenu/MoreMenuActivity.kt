bf0eb26ad8447a2a4ad676000f11ffe10799d92e
JeonK1 <kan5016@naver.com>
2026-04-01 00:49:24 +0900

    🛠 신규 화면에 대해 edge-to-edge 대응

--- app/src/main/java/com/mashup/ui/moremenu/MoreMenuActivity.kt
+++ app/src/main/java/com/mashup/ui/moremenu/MoreMenuActivity.kt
@@ -3,10 +3,11 @@ package com.mashup.ui.moremenu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -38,6 +39,7 @@ class MoreMenuActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -52,16 +54,15 @@ class MoreMenuActivity : ComponentActivity() {
         setContent {
             MashUpTheme {
                 val moreMenuState by moreMenuViewModel.moreMenuState.collectAsState()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MoreMenuRoute(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        moreMenuState = moreMenuState,
-                        onBackPressed = moreMenuViewModel::onClickBackButton,
-                        onClickMenu = moreMenuViewModel::onClickMenuButton
-                    )
-                }
+                MoreMenuRoute(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .navigationBarsPadding(),
+                    moreMenuState = moreMenuState,
+                    onBackPressed = moreMenuViewModel::onClickBackButton,
+                    onClickMenu = moreMenuViewModel::onClickMenuButton
+                )
             }
         }
     }

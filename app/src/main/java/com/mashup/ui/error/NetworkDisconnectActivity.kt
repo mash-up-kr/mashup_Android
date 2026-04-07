9004af25dfd808d643838bf444785a2f7db02e8a
citytexi <heekyne66@gmail.com>
2026-04-07 21:40:42 +0900

    🛠 network disconnect activity compose 전환

--- app/src/main/java/com/mashup/ui/error/NetworkDisconnectActivity.kt
+++ app/src/main/java/com/mashup/ui/error/NetworkDisconnectActivity.kt
@@ -2,33 +2,34 @@ package com.mashup.ui.error
 
 import android.content.Context
 import android.content.Intent
-import com.mashup.R
-import com.mashup.base.BaseActivity
-import com.mashup.databinding.ActivityNetworkDisconnectBinding
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.mashup.base.BaseComposeActivity
+import com.mashup.ui.error.route.NetworkDisconnectRoute
+import com.mashup.ui.error.vm.NetworkDisconnectViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NetworkDisconnectActivity : BaseActivity<ActivityNetworkDisconnectBinding>() {
+class NetworkDisconnectActivity : BaseComposeActivity() {
+    private val viewModel: NetworkDisconnectViewModel by viewModels()
 
-    override fun initViews() {
-        super.initViews()
-        initButtons()
-    }
-
-    private fun initButtons() {
-        viewBinding.btnClose.setOnClickListener {
-            finish()
+    @Composable
+    override fun MainContainer(modifier: Modifier) {
+        LaunchedEffect(viewModel.sideEffect) {
+            viewModel.sideEffect.collect { effect ->
+                when (effect) {
+                    is NetworkDisconnectViewModel.Effect.FinishView -> finish()
+                }
+            }
         }
-        viewBinding.btnRetry.setButtonText(
-            getString(R.string.text_button_retry)
+
+        NetworkDisconnectRoute(
+            viewModel = viewModel,
+            modifier = modifier
         )
-        viewBinding.btnRetry.setOnClickListener {
-            finish()
-        }
     }
-
-    override val layoutId: Int
-        get() = R.layout.activity_network_disconnect
 
     companion object {
         fun newIntent(context: Context): Intent {

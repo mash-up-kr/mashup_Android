e990670200b90cb45d51692252d54c1156c9fec1
Chea-yunzi <katete051@naver.com>
2026-04-21 13:59:36 +0900

    Refactor: simplify WebView URL logic and relocate URL construction to Activity
    - Remove dynamic URL prefixing and platform suffix logic from `WebViewViewModel`.
    - Pre-construct full URLs in `BirthdayActivity` and `MashongActivity` using `WEB_HOST`.
    - Inject `UserPreferenceRepository` into `MashongActivity` to append platform information to the URL before initialization.

--- app/src/main/java/com/mashup/ui/webview/mashong/MashongActivity.kt
+++ app/src/main/java/com/mashup/ui/webview/mashong/MashongActivity.kt
@@ -12,58 +12,45 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.lifecycleScope
 import com.mashup.constant.EXTRA_TITLE_KEY
-import com.mashup.constant.EXTRA_URL_KEY
 import com.mashup.core.common.bridge.MashupBridge
 import com.mashup.core.ui.theme.MashUpTheme
 import com.mashup.data.network.WEB_HOST
-import com.mashup.datastore.data.repository.UserPreferenceRepository
 import com.mashup.ui.danggn.ShakeDanggnActivity
 import com.mashup.ui.webview.WebViewScreen
 import com.mashup.ui.webview.WebViewUiState
-import com.mashup.ui.webview.WebViewViewModel
 import com.mashup.util.setFullScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+
 @AndroidEntryPoint
 class MashongActivity : ComponentActivity() {
 
-    private val webViewViewModel by viewModels<WebViewViewModel>()
-
-    @Inject
-    lateinit var userPreferenceRepository: UserPreferenceRepository
+    private val mashongViewModel by viewModels<MashongViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        lifecycleScope.launch {
-            val prefs = userPreferenceRepository.getUserPreference().first()
-            intent.putExtra(EXTRA_URL_KEY, WEB_HOST + "mashong/" + prefs.platform)
-
-            setContent {
-                MashUpTheme {
-                    val webViewUiState by webViewViewModel.webViewUiState.collectAsState(
-                        WebViewUiState.Loading
-                    )
-                    WebViewScreen(
-                        modifier = Modifier.fillMaxSize(),
-                        webViewUiState = webViewUiState,
-                        mashupBridge = MashupBridge(
-                            this@MashongActivity,
-                            onBackPressed = ::finish,
-                            onNavigateDanggn = {
-                                startActivity(ShakeDanggnActivity.newIntent(this@MashongActivity))
-                            }
-                        ),
-                        isShowMashUpToolbar = false
-                    )
-                }
+        setContent {
+            MashUpTheme {
+                val webViewUiState by mashongViewModel.webViewUiState.collectAsState(
+                    WebViewUiState.Loading
+                )
+                WebViewScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    webViewUiState = webViewUiState,
+                    mashupBridge = MashupBridge(
+                        this@MashongActivity,
+                        onBackPressed = ::finish,
+                        onNavigateDanggn = {
+                            startActivity(ShakeDanggnActivity.newIntent(this@MashongActivity))
+                        }
+                    ),
+                    isShowMashUpToolbar = false
+                )
             }
         }
+
         setFullScreen()
     }
 
@@ -79,6 +66,7 @@ class MashongActivity : ComponentActivity() {
         fun newIntent(context: Context): Intent =
             Intent(context, MashongActivity::class.java).apply {
                 putExtra(EXTRA_TITLE_KEY, "mashong")
+                putExtra(MashongViewModel.EXTRA_MASHONG_BASE_URL_KEY, WEB_HOST + "mashong/")
             }
     }
 }
